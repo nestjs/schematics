95a2a0014eda82748485d2f86820de6007676c13
thomrick <tricart@xebia.fr>
2018-02-27 13:37:14 +0100

    #guard : schematics generate a empty guard file

--- src/guard/factory.ts
+++ src/guard/factory.ts
@@ -2,5 +2,8 @@ import { Rule, Tree } from '@angular-devkit/schematics';
 import { GuardOptions } from '../../test/guard/factory.test';
 
 export function main(options: GuardOptions): Rule {
-  return (tree: Tree) => tree;
+  return (tree: Tree) => {
+    tree.create(`${ options.rootDir }/${ options.path }/${ options.name }.guard.${ options.extension }`, '');
+    return tree;
+  };
 }
--- test/guard/factory.test.ts
+++ test/guard/factory.test.ts
@@ -1,11 +1,22 @@
 import { VirtualTree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { expect } from 'chai';
 import * as path from 'path';
 
-export interface GuardOptions {}
+export interface GuardOptions {
+  extension: string;
+  name: string;
+  path: string;
+  rootDir: string;
+}
 
 describe('Guard Factory', () => {
-  const options: GuardOptions = {};
+  const options: GuardOptions = {
+    extension: 'ts',
+    name: 'name',
+    path: 'path',
+    rootDir: 'src/modules'
+  };
   let runner: SchematicTestRunner;
   beforeEach(() => {
     runner = new SchematicTestRunner(
@@ -13,7 +24,13 @@ describe('Guard Factory', () => {
       path.join(process.cwd(), 'src/collection.json')
     );
   });
-  it('can call guard schematics', () => {
-    runner.runSchematic('guard', options, new VirtualTree());
+  it('should generate a new guard file', () => {
+    const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
+    const files: string[] = tree.files;
+    expect(
+      files.find((filename) =>
+        filename === `/${ options.rootDir }/${ options.path }/${ options.name }.guard.${ options.extension }`
+      )
+    ).to.not.be.undefined;
   });
 });
