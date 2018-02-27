8422489fe662e7b8c9263421ed25c05a36fd926d
thomrick <tricart@xebia.fr>
2018-02-27 09:04:05 +0100

    #middleware : develop middleware schematics

--- src/middleware/factory.ts
+++ src/middleware/factory.ts
@@ -1,6 +1,18 @@
-import { Rule, Tree } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
+import * as path from 'path';
 import { AssetOptions } from '../schemas';
 
 export function main(options: AssetOptions): Rule {
-  return (tree: Tree) => tree;
+  return mergeWith(
+    apply(
+      url('./files'), [
+        template({
+          ...strings,
+          ...options
+        }),
+        move(path.join(options.rootDir, options.path))
+      ]
+    )
+  );
 }
--- test/middleware/factory.test.ts
+++ test/middleware/factory.test.ts
@@ -1,5 +1,6 @@
 import { VirtualTree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { expect } from 'chai';
 import * as path from 'path';
 import { AssetOptions } from '../../src/schemas';
 
@@ -17,7 +18,30 @@ describe('Middleware Factory', () => {
       path.join(process.cwd(), 'src/collection.json')
     );
   });
-  it('can call middleware schematics', () => {
-    runner.runSchematic('middleware', options, new VirtualTree());
+  it('should create a new middleware file', () => {
+    const tree: UnitTestTree = runner.runSchematic('middleware', options, new VirtualTree());
+    const files: string[] = tree.files;
+    expect(
+      files.find((filename) =>
+          filename === `/${ options.rootDir }/${ options.path }/${ options.name }.middleware.${ options.extension }`
+      )
+    ).to.not.be.undefined;
+  });
+  it('should create the expected middleware file content', () => {
+    const tree: UnitTestTree = runner.runSchematic('middleware', options, new VirtualTree());
+    expect(
+      tree.readContent(`/${ options.rootDir }/${ options.path }/${ options.name }.middleware.${ options.extension }`)
+    ).to.be.equal(
+      'import { Middleware, NestMiddleware, ExpressMiddleware } from \'@nestjs/common\';\n' +
+      '\n' +
+      '@Middleware()\n' +
+      'export class NameMiddleware implements NestMiddleware {\n' +
+      '  resolve(...args: any[]): ExpressMiddleware {\n' +
+      '    return (req, res, next) => {\n' +
+      '      next();\n' +
+      '    };\n' +
+      '  }\n' +
+      '}\n'
+    );
   });
 });
