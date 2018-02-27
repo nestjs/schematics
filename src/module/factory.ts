25744aa2153a96d6e5387b5a6dada6512cd3d57c
thomrick <tricart@xebia.fr>
2018-02-27 09:10:18 +0100

    #middleware : refactor module schematics implementation

--- src/module/factory.ts
+++ src/module/factory.ts
@@ -1,18 +1,18 @@
 import { strings } from '@angular-devkit/core';
 import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
+import * as path from 'path';
 import { AssetOptions } from '../schemas';
 
 export function main(options: AssetOptions): Rule {
   return mergeWith(
     apply(
-      url('./files'),
-      [
+      url('./files'), [
         template({
           ...strings,
-          ...options,
+          ...options
         }),
-        move(options.rootDir),
+        move(path.join(options.rootDir, options.path))
       ]
-    ),
+    )
   );
 }
