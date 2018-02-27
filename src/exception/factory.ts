302f73d23a716465ca83a727258c242d74710a81
thomrick <tricart@xebia.fr>
2018-02-27 09:53:17 +0100

    #exception : develop exception file generation

--- src/exception/factory.ts
+++ src/exception/factory.ts
@@ -1,5 +1,9 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
+import { ExceptionOptions } from './schema';
 
-export function main(options: any): Rule {
-  return (tree: Tree) => tree;
+export function main(options: ExceptionOptions): Rule {
+  return (tree: Tree) => {
+    tree.create(`${ options.rootDir }/${ options.path }/${ options.name }.exception.${ options.extension }`, '');
+    return tree;
+  };
 }
--- test/exception/factory.test.ts
+++ test/exception/factory.test.ts
@@ -1,9 +1,16 @@
 import { VirtualTree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { expect } from 'chai';
 import * as path from 'path';
+import { ExceptionOptions } from '../../src/exception/schema';
 
 describe('Exception Factory', () => {
-  const options: any = {};
+  const options: ExceptionOptions = {
+    extension: 'ts',
+    name: 'name',
+    path: 'path',
+    rootDir: 'src/modules'
+  };
   let runner: SchematicTestRunner;
   beforeEach(() => {
     runner = new SchematicTestRunner(
@@ -11,7 +18,13 @@ describe('Exception Factory', () => {
       path.join(process.cwd(), 'src/collection.json')
     );
   });
-  it('can call exception schematics', () => {
-    runner.runSchematic('exception', options, new VirtualTree());
+  it('should generate a new exception file', () => {
+    const tree: UnitTestTree = runner.runSchematic('exception', options, new VirtualTree());
+    const files: string[] = tree.files;
+    expect(
+      files.find((filename) =>
+        filename === `/${ options.rootDir }/${ options.path }/${ options.name }.exception.${ options.extension }`
+      )
+    ).to.not.be.undefined;
   });
 });
