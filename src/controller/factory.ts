9d3903a3f769b1a8a7533e229ede4b11a055e307
thomrick <tricart@xebia.fr>
2018-02-27 09:12:45 +0100

    #middleware : refactor controller schematics implementation

--- src/controller/factory.ts
+++ src/controller/factory.ts
@@ -1,17 +1,17 @@
 import { strings } from '@angular-devkit/core';
 import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
+import * as path from 'path';
 import { AssetOptions } from '../schemas';
 
 export function main(options: AssetOptions): Rule {
   return mergeWith(
     apply(
-      url('./files'),
-      [
+      url('./files'), [
         template({
           ...strings,
           ...options
         }),
-        move(options.rootDir),
+        move(path.join(options.rootDir, options.path))
       ]
     )
   );
