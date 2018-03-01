88d69a0697a10131f765261a2e3d673dae2cc1c5
thomrick <tricart@xebia.fr>
2018-03-01 17:08:03 +0100

    #application-files : fix lint errors

--- test/application/factory.test.ts
+++ test/application/factory.test.ts
@@ -6,9 +6,9 @@ import { ApplicationOptions } from '../../src/application/schema';
 
 describe('Application Factory', () => {
   const options: ApplicationOptions = {
-    'directory': 'directory',
-    'name': 'name',
-    'extension': 'ts'
+    directory: 'directory',
+    name: 'name',
+    extension: 'ts'
   };
   let tree: UnitTestTree;
   beforeEach(() => {
@@ -20,72 +20,72 @@ describe('Application Factory', () => {
   });
   it('should generate Nest application files',
     () => {
-    const files: string[] = tree.files;
-    expect(files.find((filename) => filename === `/${ options.directory }/package.json`))
-      .to.not.be.undefined;
-    expect(files.find((filename) => filename === `/${ options.directory }/.nest-cli.json`))
-      .to.not.be.undefined;
-    expect(files.find((filename) => filename === `/${ options.directory }/src/main.ts`))
-      .to.not.be.undefined;
-    expect(files.find((filename) => filename === `/${ options.directory }/src/application.module.ts`))
-      .to.not.be.undefined;
-    expect(files.find((filename) => filename === `/${ options.directory }/tsconfig.json`))
-      .to.not.be.undefined;
-    expect(files.find((filename) => filename === `/${ options.directory }/tslint.json`))
-      .to.not.be.undefined;
-  });
+      const files: string[] = tree.files;
+      expect(files.find((filename) => filename === `/${ options.directory }/package.json`))
+        .to.not.be.undefined;
+      expect(files.find((filename) => filename === `/${ options.directory }/.nest-cli.json`))
+        .to.not.be.undefined;
+      expect(files.find((filename) => filename === `/${ options.directory }/src/main.ts`))
+        .to.not.be.undefined;
+      expect(files.find((filename) => filename === `/${ options.directory }/src/application.module.ts`))
+        .to.not.be.undefined;
+      expect(files.find((filename) => filename === `/${ options.directory }/tsconfig.json`))
+        .to.not.be.undefined;
+      expect(files.find((filename) => filename === `/${ options.directory }/tslint.json`))
+        .to.not.be.undefined;
+    });
   it(`should generate the right '${ options.directory }/package.json' file content`,
     () => {
-    expect(tree.readContent(`/${ options.directory }/package.json`))
-      .to.be.equal(
-      JSON.stringify(
-        {
-          'name': options.name,
-          'version': '0.0.0',
-          'license': 'MIT',
-          'scripts': {
-            'test': 'echo "Error: no test specified" && exit 1'
+      expect(tree.readContent(`/${ options.directory }/package.json`))
+        .to.be.equal(
+        JSON.stringify(
+          {
+            name: options.name,
+            version: '0.0.0',
+            license: 'MIT',
+            scripts: {
+              test: 'echo "Error: no test specified" && exit 1'
+            },
+            dependencies: {
+              '@nestjs/common': '^4.5.9',
+              '@nestjs/core': '^4.5.10',
+              'reflect-metadata': '^0.1.12',
+              'rxjs': '^5.5.6'
+            },
+            devDependencies: {
+              '@nestjs/testing': '^4.5.5',
+              '@types/node': '^9.3.0',
+              'ts-node': '^4.1.0',
+              'typescript': '^2.6.2'
+            }
           },
-          'dependencies': {
-            '@nestjs/common': '^4.5.9',
-            '@nestjs/core': '^4.5.10',
-            'reflect-metadata': '^0.1.12',
-            'rxjs': '^5.5.6'
-          },
-          'devDependencies': {
-            '@nestjs/testing': '^4.5.5',
-            '@types/node': '^9.3.0',
-            'ts-node': '^4.1.0',
-            'typescript': '^2.6.2'
-          }
-        },
-        null,
-        2)
-    );
-  });
+          null,
+          2)
+      );
+    });
   it(`should generate the right '${ options.directory }/.nest-cli.json' file content`,
     () => {
-    expect(tree.readContent(`/${ options.directory }/.nest-cli.json`))
-      .to.be.equal(
-      JSON.stringify(
-        {
-          'language': options.extension,
-          'project': {
-            'name': options.directory
+      expect(tree.readContent(`/${ options.directory }/.nest-cli.json`))
+        .to.be.equal(
+        JSON.stringify(
+          {
+            language: options.extension,
+            project: {
+              name: options.directory
+            },
+            app: {
+              root: 'src',
+              main: `main.${ options.extension }`
+            }
           },
-          'app': {
-            'root': 'src',
-            'main': `main.${ options.extension }`
-          }
-        },
-        null,
-        2)
-    );
-  });
+          null,
+          2)
+      );
+    });
   it(`should generate the right '${ options.directory }/src/main.ts' file content`,
     () => {
-    expect(tree.readContent(`/${ options.directory }/src/main.ts`))
-      .to.be.equal(
+      expect(tree.readContent(`/${ options.directory }/src/main.ts`))
+        .to.be.equal(
         'import { NestFactory } from \'@nestjs/core\';\n' +
         'import { ApplicationModule } from \'./application.module\';\n' +
         '\n' +
@@ -94,30 +94,30 @@ describe('Application Factory', () => {
         '\tawait app.listen(3000);\n' +
         '}\n' +
         'bootstrap();\n'
-    );
-  });
+      );
+    });
   it(`should generate the right '${ options.directory }/src/application.module.ts' file content`,
     () => {
-    expect(tree.readContent(`/${ options.directory }/src/application.module.ts`))
-      .to.be.equal(
+      expect(tree.readContent(`/${ options.directory }/src/application.module.ts`))
+        .to.be.equal(
         'import { Module } from \'@nestjs/common\';\n' +
         '\n' +
         '@Module({})\n' +
         'export class ApplicationModule {}\n'
-    );
-  });
+      );
+    });
   it(`should generate the right '${ options.directory }/tsconfig.json' file content`,
     () => {
       expect(tree.readContent(`${ options.directory }/tsconfig.json`))
         .to.be.equal(
-          JSON.stringify({}, null, 2)
+        JSON.stringify({}, null, 2)
       );
     });
   it(`should generate the right '${ options.directory }/tslint.json' file content`,
     () => {
       expect(tree.readContent(`${ options.directory }/tslint.json`))
         .to.be.equal(
-          JSON.stringify({}, null, 2)
+        JSON.stringify({}, null, 2)
       );
     });
 });
