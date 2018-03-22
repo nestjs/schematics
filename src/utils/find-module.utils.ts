20650b9e901abc9696a72003472daf068d70cb25
thomrick <tricart@xebia.fr>
2018-03-22 14:34:36 +0100

    #nest-v4 : rename module.utils.ts in find-module.utils.ts

--- src/utils/find-module.utils.ts
+++ src/utils/find-module.utils.ts
@@ -1,7 +1,7 @@
 import { normalize } from '@angular-devkit/core';
 import { Tree } from '@angular-devkit/schematics';
 
-export class ModuleUtils {
+export class FindModuleUtils {
   public static find(tree: Tree, moduleName: string): string {
     return normalize(`/src/${ moduleName }.module.ts`);
   }
--- test/utils/find-module.utils.test.ts
+++ test/utils/find-module.utils.test.ts
@@ -2,15 +2,15 @@ import { VirtualTree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import { expect } from 'chai';
 import * as path from 'path';
-import { ModuleUtils } from '../../src/utils/module.utils';
+import { FindModuleUtils } from '../../src/utils/find-module.utils';
 
-describe('Module Utils', () => {
+describe('Find Module Utils', () => {
   it('should return the app module path in application tree', () => {
     const runner: SchematicTestRunner = new SchematicTestRunner(
       '.',
       path.join(process.cwd(), 'src/collection.json')
     );
     const tree: UnitTestTree = runner.runSchematic('application', { directory: 'directory' }, new VirtualTree());
-    expect(ModuleUtils.find(tree, 'app')).to.be.equal('/src/app.module.ts');
+    expect(FindModuleUtils.find(tree, 'app')).to.be.equal('/src/app.module.ts');
   });
 });
