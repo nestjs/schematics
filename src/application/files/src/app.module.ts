2695a0b2a7abc7ad4e8ea03f40b3ff86da4b1910
thomrick <tricart@xebia.fr>
2018-03-22 09:22:48 +0100

    #nest-v4 : update initial project components

--- src/application/files/src/app.module.ts
+++ src/application/files/src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 
-@Module({})
+@Module({
+  imports: [],
+  controllers: [AppController],
+  components: [],
+})
 export class ApplicationModule {}
