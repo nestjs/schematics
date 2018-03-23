ac607495e78cb44cb89c28d103ae2e12516fb8c2
thomrick <tricart@xebia.fr>
2018-03-23 16:30:08 +0100

    #nest-v5 : review controller generation

--- src/controller/factory.ts
+++ src/controller/factory.ts
@@ -1,6 +1,5 @@
 import { strings } from '@angular-devkit/core';
 import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
-import * as path from 'path';
 import { ControllerOptions } from './schema';
 
 export function main(options: ControllerOptions): Rule {
@@ -11,7 +10,7 @@ export function main(options: ControllerOptions): Rule {
           ...strings,
           ...options
         }),
-        move(path.join(options.rootDir, options.path))
+        move('/src')
       ]
     )
   );
