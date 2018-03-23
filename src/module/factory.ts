c644c671f21070a78f36b2a0cdfb84ae3a940e28
thomrick <tricart@xebia.fr>
2018-03-23 16:03:03 +0100

    #nest-v5 : add app module updates on generate module

--- src/module/factory.ts
+++ src/module/factory.ts
@@ -1,17 +1,42 @@
-import { strings } from '@angular-devkit/core';
-import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
+import { basename, dirname, normalize, Path, relative, strings } from '@angular-devkit/core';
+import {
+  apply, chain, mergeWith, move, Rule, SchematicContext, Source, template, Tree,
+  url
+} from '@angular-devkit/schematics';
 import { ModuleOptions } from './schema';
+import { classify } from '@angular-devkit/core/src/utils/strings';
+import { ModuleImportUtils } from '../utils/module-import.utils';
+import { ModuleMetadataUtils } from '../utils/module-metadata.utils';
 
 export function main(options: ModuleOptions): Rule {
-  return mergeWith(
-    apply(
-      url('./files'), [
-        template({
-          ...strings,
-          ...options
-        }),
-        move('src')
-      ]
-    )
+  return (tree: Tree, context: SchematicContext) => {
+    return chain([
+      mergeWith(generate(options)),
+      addDeclarationToModule(options)
+    ])(tree, context);
+  };
+}
+
+function generate(options: ModuleOptions): Source {
+  return apply(
+    url('./files'), [
+      template({
+        ...strings,
+        ...options
+      }),
+      move('src')
+    ]
   );
+}
+
+function addDeclarationToModule(options: ModuleOptions): Rule {
+  return (tree: Tree) => {
+    const symbol: string = `${ classify(options.name) }Module`;
+    const relativePath: string = `./${ options.name }/${ options.name }.module`;
+    let content = tree.read('/src/app.module.ts').toString();
+    content = ModuleImportUtils.insert(content, symbol, relativePath);
+    content = ModuleMetadataUtils.insert(content, symbol);
+    tree.overwrite('/src/app.module.ts', content);
+    return tree;
+  };
 }
