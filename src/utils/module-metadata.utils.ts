607cbe8407030c2c2a397a9e2e8e8844ae2cc797
thomrick <tricart@xebia.fr>
2018-03-23 11:33:43 +0100

    #nest-v5 : adapte module metadata utils to add metadata

--- src/utils/module-metadata.utils.ts
+++ src/utils/module-metadata.utils.ts
@@ -1,9 +1,29 @@
+export interface ModuleMetadata {
+  imports?: any[];
+}
+
 export class ModuleMetadataUtils {
+  private static METADATA_REGEXP: RegExp = /@Module\((.*)\)/;
+
   public static insert(source: string, symbol: string): string {
-    const importsLine: string = source
-      .split('\n')
-      .find((line) => line.match(/imports/))
-      .replace(/\[\]/, `[ ${ symbol } ],\n`);
-    return source.replace(/imports/, importsLine);
+    const metadata: ModuleMetadata = this.extract(source);
+    return source
+      .replace(
+        this.METADATA_REGEXP,
+        `@Module(${ JSON.stringify(this.addSymbol(metadata, symbol), null, 2) })`
+      );
+  }
+
+  private static extract(source: string): ModuleMetadata {
+    const results: RegExpExecArray | null = this.METADATA_REGEXP.exec(source);
+    return <ModuleMetadata> JSON.parse(results[1]);
+  }
+
+  private static addSymbol(metadata: ModuleMetadata, symbol: string): ModuleMetadata {
+    const updatedMetadata: ModuleMetadata = Object.assign({}, metadata);
+    if (updatedMetadata.imports === undefined) {
+      updatedMetadata.imports = [ symbol ];
+    }
+    return updatedMetadata;
   }
 }
--- test/utils/module-metadata.utils.test.ts
+++ test/utils/module-metadata.utils.test.ts
@@ -2,17 +2,33 @@ import { expect } from 'chai';
 import { ModuleMetadataUtils } from '../../src/utils/module-metadata.utils';
 
 describe('Module Metadata Utils', () => {
-  const source =
-    'import { Module } from \'@nestjs/common\';\n' +
-    'import { AppController } from \'./app.controller\';\n' +
-    '\n' +
-    '@Module({\n' +
-    '  imports: [],\n' +
-    '  controllers: [AppController],\n' +
-    '  components: [],\n' +
-    '})\n' +
-    'export class ApplicationModule {}\n';
-  it('should add a module to modules metadata', () => {
+  it('should create the imports metadata if not exist when insert', () => {
+    const source =
+      'import { Module } from \'@nestjs/common\';\n' +
+      'import { AppController } from \'./app.controller\';\n' +
+      '\n' +
+      '@Module({})\n' +
+      'export class ApplicationModule {}\n';
+    const output: string = ModuleMetadataUtils.insert(source, 'FooModule');
+    expect(output).to.be.equal(
+      'import { Module } from \'@nestjs/common\';\n' +
+      'import { AppController } from \'./app.controller\';\n' +
+      '\n' +
+      `@Module(${ JSON.stringify({ imports: [ "FooModule" ]}, null, 2)})\n` +
+      'export class ApplicationModule {}\n'
+    );
+  });
+  it.skip('should add a module to modules metadata', () => {
+    const source =
+      'import { Module } from \'@nestjs/common\';\n' +
+      'import { AppController } from \'./app.controller\';\n' +
+      '\n' +
+      '@Module({\n' +
+      '  imports: [],\n' +
+      '  controllers: [AppController],\n' +
+      '  components: [],\n' +
+      '})\n' +
+      'export class ApplicationModule {}\n';
     const output: string = ModuleMetadataUtils.insert(source, 'FooModule');
     expect(output).to.match(/imports: \[ FooModule \]/);
   });
