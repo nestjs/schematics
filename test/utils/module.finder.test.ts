32ef8ec928b323c3e5a2c025b6a6da037b172349
thomrick <tricart@xebia.fr>
2018-03-24 20:40:46 +0100

    #nest-v5 : develop a module finder to replace module find utils

---
+++ test/utils/module.finder.test.ts
@@ -0,0 +1,46 @@
+import { EmptyTree } from '@angular-devkit/schematics';
+import { expect } from 'chai';
+import { ModuleFinder, Options } from '../../src/utils/module.finder';
+
+describe('Module Finder', () => {
+  it('should return the app module path', () => {
+    const tree = new EmptyTree();
+    tree.create('/src/app.module.ts', 'app module content');
+    tree.create('/src/name/name.controller.ts', 'name controller content');
+    const finder = new ModuleFinder(tree);
+    const options: Options = {
+      name: 'name',
+      path: 'name',
+      kind: 'controller'
+    };
+    expect(finder.find(options)).to.be.equal('/src/app.module.ts');
+  });
+  it('should return the generated directory module path', () => {
+    const tree = new EmptyTree();
+    tree.create('/src/app.module.ts', 'app module content');
+    tree.create('/src/name/name.module.ts', 'name module content');
+    tree.create('/src/name/name.controller.ts', 'name controller content');
+    const finder = new ModuleFinder(tree);
+    const options: Options = {
+      name: 'name',
+      path: 'name',
+      kind: 'controller'
+    };
+    expect(finder.find(options))
+      .to.equal('/src/name/name.module.ts');
+  });
+  it('should return the intermediate module path', () => {
+    const tree = new EmptyTree();
+    tree.create('/src/app.module.ts', 'app module content');
+    tree.create('/src/nested/nested.module.ts', 'nested module content');
+    tree.create('/src/nested/name/name.module.ts', 'name module content');
+    const finder = new ModuleFinder(tree);
+    const options: Options = {
+      name: 'name',
+      path: 'nested/name',
+      kind: 'module'
+    };
+    expect(finder.find(options))
+      .to.equal('/src/nested/nested.module.ts');
+  });
+});
