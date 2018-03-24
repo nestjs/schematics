4c130b0150ca19a6fc714a46ab04ed8e148d1aad
thomrick <tricart@xebia.fr>
2018-03-24 11:59:59 +0100

    #nest-v5 : import service into app.module

--- src/service/factory.ts
+++ src/service/factory.ts
@@ -1,17 +1,38 @@
 import { strings } from '@angular-devkit/core';
-import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
+import { apply, chain, mergeWith, move, Rule, template, Tree, url } from '@angular-devkit/schematics';
 import { ServiceOptions } from './schema';
+import { ModuleMetadataUtils } from '../utils/module-metadata.utils';
+import { ModuleImportUtils } from '../utils/module-import.utils';
+import { ModuleOptions } from '../module/schema';
+import { classify } from '@angular-devkit/core/src/utils/strings';
 
 export function main(options: ServiceOptions): Rule {
-  return mergeWith(
-    apply(
-      url('./files'), [
-        template({
-          ...strings,
-          ...options
-        }),
-        move('/src')
-      ]
-    )
+  return chain([
+    mergeWith(generate(options)),
+    addDeclarationToModule(options)
+  ]);
+}
+
+function generate(options: ServiceOptions) {
+  return apply(
+    url('./files'), [
+      template({
+        ...strings,
+        ...options
+      }),
+      move('/src')
+    ]
   );
+}
+
+function addDeclarationToModule(options: ModuleOptions): Rule {
+  return (tree: Tree) => {
+    const symbol: string = `${ classify(options.name) }Service`;
+    const relativePath: string = `./${ options.name }/${ options.name }.service`;
+    let content = tree.read('/src/app.module.ts').toString();
+    content = ModuleImportUtils.insert(content, symbol, relativePath);
+    content = ModuleMetadataUtils.insert(content, 'components', symbol);
+    tree.overwrite('/src/app.module.ts', content);
+    return tree;
+  };
 }
