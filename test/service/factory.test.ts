e2744cb85ec7f3cc7d87bdd333452e8e0648899b
thomrick <tricart@xebia.fr>
2018-03-24 11:27:54 +0100

    #nest-v5 : refactor service schematics and unit tests + rename schema.ts in schema.d.ts

--- test/service/factory.test.ts
+++ test/service/factory.test.ts
@@ -6,33 +6,27 @@ import { ServiceOptions } from '../../src/service/schema';
 
 describe('Service Factory', () => {
   const options: ServiceOptions = {
-    extension: 'ts',
     name: 'name',
-    path: 'name',
-    rootDir: 'src/modules'
   };
-  let runner: SchematicTestRunner;
+  let tree: UnitTestTree;
   beforeEach(() => {
-    runner = new SchematicTestRunner(
+    const runner: SchematicTestRunner = new SchematicTestRunner(
       '.',
       path.join(process.cwd(), 'src/collection.json')
     );
+    tree = runner.runSchematic('service', options, new VirtualTree());
   });
   it('should generate a new service file', () => {
-    const tree: UnitTestTree = runner.runSchematic('service', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find(
-        (filename) => filename === `/src/modules/${ options.path }/${ options.name }.service.${ options.extension }`
+        (filename) => filename === `/src/${ options.name }.service.ts`
       )
     ).to.not.be.undefined;
   });
   it('should generate the expected service file content', () => {
-    const tree: UnitTestTree = runner.runSchematic('service', options, new VirtualTree());
     expect(
-      tree
-        .read(`/${ options.rootDir }/${ options.path }/${ options.name }.service.${ options.extension }`)
-        .toString()
+      tree.readContent(`/src/${ options.name }.service.ts`)
     ).to.be.equal(
       'import { Component } from \'@nestjs/common\';\n' +
       '\n' +
