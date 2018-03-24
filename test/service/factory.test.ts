b7d9266bb07d40eb5e2e68a39a83056ef2383c38
thomrick <tricart@xebia.fr>
2018-03-24 17:10:35 +0100

    #nest-v5 : update module in generated service schematics

--- test/service/factory.test.ts
+++ test/service/factory.test.ts
@@ -4,62 +4,101 @@ import { expect } from 'chai';
 import * as path from 'path';
 import { ApplicationOptions } from '../../src/application/schema';
 import { ServiceOptions } from '../../src/service/schema';
+import { ControllerOptions } from '../../src/controller/schema';
+import { ModuleOptions } from '../../src/module/schema';
 
 describe('Service Factory', () => {
-  const options: ServiceOptions = {
-    name: 'name',
-  };
-  let tree: UnitTestTree;
-  before(() => {
-    const runner: SchematicTestRunner = new SchematicTestRunner(
-      '.',
-      path.join(process.cwd(), 'src/collection.json')
-    );
-    const appOptions: ApplicationOptions = {
-      directory: '',
+  describe('No module in controller generate directory', () => {
+    const options: ServiceOptions = {
+      name: 'name',
     };
-    const appTree: UnitTestTree = runner.runSchematic('application', appOptions, new VirtualTree());
-    tree = runner.runSchematic('service', options, appTree);
+    let tree: UnitTestTree;
+    before(() => {
+      const runner: SchematicTestRunner = new SchematicTestRunner(
+        '.',
+        path.join(process.cwd(), 'src/collection.json')
+      );
+      const appOptions: ApplicationOptions = {
+        directory: '',
+      };
+      const appTree: UnitTestTree = runner.runSchematic('application', appOptions, new VirtualTree());
+      tree = runner.runSchematic('service', options, appTree);
+    });
+    it('should generate a new service file', () => {
+      const files: string[] = tree.files;
+      expect(
+        files.find(
+          (filename) => filename === `/src/${options.name}/${ options.name }.service.ts`
+        )
+      ).to.not.be.undefined;
+    });
+    it('should generate the expected service file content', () => {
+      expect(
+        tree.readContent(`/src/${options.name}/${ options.name }.service.ts`)
+      ).to.be.equal(
+        'import { Component } from \'@nestjs/common\';\n' +
+        '\n' +
+        '@Component()\n' +
+        'export class NameService {}\n'
+      );
+    });
+    it('should import the new service in the application module', () => {
+      expect(
+        tree.readContent(path.join(
+          '/src',
+          'app.module.ts'
+        ))
+      ).to.be.equal(
+        'import { Module } from \'@nestjs/common\';\n' +
+        'import { AppController } from \'./app.controller\';\n' +
+        'import { NameService } from \'./name/name.service\';\n' +
+        '\n' +
+        '@Module({\n' +
+        '  imports: [],\n' +
+        '  controllers: [\n' +
+        '    AppController\n' +
+        '  ],\n' +
+        '  components: [\n' +
+        '    NameService\n' +
+        '  ]\n' +
+        '})\n' +
+        'export class ApplicationModule {}\n'
+      );
+    });
   });
-  it('should generate a new service file', () => {
-    const files: string[] = tree.files;
-    expect(
-      files.find(
-        (filename) => filename === `/src/${options.name}/${ options.name }.service.ts`
-      )
-    ).to.not.be.undefined;
-  });
-  it('should generate the expected service file content', () => {
-    expect(
-      tree.readContent(`/src/${options.name}/${ options.name }.service.ts`)
-    ).to.be.equal(
-      'import { Component } from \'@nestjs/common\';\n' +
-      '\n' +
-      '@Component()\n' +
-      'export class NameService {}\n'
-    );
-  });
-  it('should import the new service in the application module', () => {
-    expect(
-      tree.readContent(path.join(
-        '/src',
-        'app.module.ts'
-      ))
-    ).to.be.equal(
-      'import { Module } from \'@nestjs/common\';\n' +
-      'import { AppController } from \'./app.controller\';\n' +
-      'import { NameService } from \'./name/name.service\';\n' +
-      '\n' +
-      '@Module({\n' +
-      '  imports: [],\n' +
-      '  controllers: [\n' +
-      '    AppController\n' +
-      '  ],\n' +
-      '  components: [\n' +
-      '    NameService\n' +
-      '  ]\n' +
-      '})\n' +
-      'export class ApplicationModule {}\n'
-    );
+  describe('Generated module before controller generation', () => {
+    const options: ServiceOptions = {
+      name: 'name',
+    };
+    let tree: UnitTestTree;
+    before(() => {
+      const runner: SchematicTestRunner = new SchematicTestRunner(
+        '.',
+        path.join(process.cwd(), 'src/collection.json')
+      );
+      const appOptions: ApplicationOptions = {
+        directory: '',
+      };
+      let root: UnitTestTree = runner.runSchematic('application', appOptions, new VirtualTree());
+      const moduleOptions: ModuleOptions = {
+        name: options.name
+      };
+      root = runner.runSchematic('module', moduleOptions, root);
+      tree = runner.runSchematic('service', options, root);
+    });
+    it('should import the new controller in the generated directory module', () => {
+      expect(tree.readContent(`/src/${options.name}/${options.name}.module.ts`))
+        .to.be.equal(
+        'import { Module } from \'@nestjs/common\';\n' +
+        'import { NameService } from \'./name.service\';\n' +
+        '\n' +
+        '@Module({\n' +
+        '  components: [\n' +
+        '    NameService\n' +
+        '  ]\n' +
+        '})\n' +
+        'export class NameModule {}\n'
+      );
+    });
   });
 });
