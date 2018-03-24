0d0de7467e5b1847790fa1bc09a95c24be21a969
thomrick <tricart@xebia.fr>
2018-03-24 20:40:46 +0100

    #nest-v4 : develop a module finder to replace module find utils

---
+++ src/utils/module.finder.ts
@@ -0,0 +1,42 @@
+import { join, normalize, Path, PathFragment, split } from '@angular-devkit/core';
+import { DirEntry, Tree } from '@angular-devkit/schematics';
+
+export interface Options {
+  name?: string;
+  path: string;
+  kind: 'module' | 'controller' | 'service';
+}
+
+export class ModuleFinder {
+  private readonly ROOT_PATH: Path = normalize('/src');
+
+  constructor(private tree: Tree) {}
+
+  public find(options: Options): string {
+    const generatedDirectoryPath: Path = join(this.ROOT_PATH, normalize(options.path));
+    const generatedDirectory: DirEntry = this.tree.getDir(generatedDirectoryPath);
+    const moduleFilename: PathFragment = generatedDirectory
+      .subfiles
+      .find((filename) =>
+        filename.includes('.module.ts')
+        &&
+        (options.kind !== 'module' || !filename.includes(options.name))
+      );
+    return moduleFilename !== undefined ?
+      join(generatedDirectoryPath, moduleFilename.valueOf())
+      :
+      this.findInParent(options);
+  }
+
+  private findInParent(options: Options): string {
+    const copyOfOptions: Options = Object.assign({}, options);
+    copyOfOptions.path = this.computeParentPath(options.path);
+    return this.find(copyOfOptions);
+  }
+
+  private computeParentPath(path: string): string {
+    const fragments: PathFragment[] = split(normalize(path));
+    fragments.pop();
+    return fragments.length === 0 ? '' : join(...fragments).valueOf();
+  }
+}
