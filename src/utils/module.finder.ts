fee5de971aa9ebc4a5f2fe4bf227913fdb8cea71
thomrick <tricart@xebia.fr>
2018-03-24 22:03:33 +0100

    #nest-v5 : refactor module finder

--- src/utils/module.finder.ts
+++ src/utils/module.finder.ts
@@ -15,7 +15,11 @@ export class ModuleFinder {
   public find(options: FindOptions): string {
     const generatedDirectoryPath: Path = join(this.ROOT_PATH, normalize(options.path));
     const generatedDirectory: DirEntry = this.tree.getDir(generatedDirectoryPath);
-    const moduleFilename: PathFragment = generatedDirectory
+    return this.findIn(generatedDirectory, options);
+  }
+
+  private findIn(directory: DirEntry, options: FindOptions): string {
+    const moduleFilename: PathFragment = directory
       .subfiles
       .find((filename) =>
         filename.includes('.module.ts')
@@ -23,20 +27,8 @@ export class ModuleFinder {
         (options.kind !== 'module' || !filename.includes(options.name))
       );
     return moduleFilename !== undefined ?
-      join(generatedDirectoryPath, moduleFilename.valueOf())
+      join(directory.path, moduleFilename.valueOf())
       :
-      this.findInParent(options);
-  }
-
-  private findInParent(options: FindOptions): string {
-    const copyOfOptions: FindOptions = Object.assign({}, options);
-    copyOfOptions.path = this.computeParentPath(options.path);
-    return this.find(copyOfOptions);
-  }
-
-  private computeParentPath(path: string): string {
-    const fragments: PathFragment[] = split(normalize(path));
-    fragments.pop();
-    return fragments.length === 0 ? '' : join(...fragments).valueOf();
+      this.findIn(directory.parent, options);
   }
 }
