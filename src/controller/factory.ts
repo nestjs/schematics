4e365a251971009fb8dd2b7215e8060388d8f862
thomrick <tricart@xebia.fr>
2018-03-24 22:31:42 +0100

    #nest-v5 : use module finder instead of find module utils in controller factory

--- src/controller/factory.ts
+++ src/controller/factory.ts
@@ -1,9 +1,9 @@
 import { normalize, Path, relative, strings } from '@angular-devkit/core';
 import { classify } from '@angular-devkit/core/src/utils/strings';
 import { apply, chain, mergeWith, move, Rule, template, Tree, url } from '@angular-devkit/schematics';
-import { ModuleFindUtils } from '../utils/module-find.utils';
 import { ModuleImportUtils } from '../utils/module-import.utils';
 import { ModuleMetadataUtils } from '../utils/module-metadata.utils';
+import { ModuleFinder } from '../utils/module.finder';
 import { ControllerOptions } from './schema';
 
 export function main(options: ControllerOptions): Rule {
@@ -28,8 +28,13 @@ function generate(options: ControllerOptions) {
 
 function addDeclarationToModule(options: ControllerOptions): Rule {
   return (tree: Tree) => {
-    const generatedDirectoryPath: string = normalize(`/src/${ options.name }`);
-    const moduleToInsertPath: string = ModuleFindUtils.find(tree, generatedDirectoryPath);
+    const finder: ModuleFinder = new ModuleFinder(tree);
+    const moduleToInsertPath: string = finder.find({
+      name: options.name,
+      path: options.path,
+      kind: 'controller'
+    });
+    const generatedDirectoryPath: string = normalize(`/src/${ options.path }`);
     let relativePath: string = relative(moduleToInsertPath as Path, generatedDirectoryPath as Path);
     relativePath = `${ relativePath.substring(1, relativePath.length) }/${ options.name }.controller`;
     let content = tree.read(moduleToInsertPath).toString();
