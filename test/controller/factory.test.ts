b02714f8384b8378dab8c6c7d7110ada367e1b2b
thomrick <tricart@xebia.fr>
2018-03-24 12:33:09 +0100

    #nest-v5 : create test to import controller into generated directory module

--- test/controller/factory.test.ts
+++ test/controller/factory.test.ts
@@ -4,66 +4,104 @@ import { expect } from 'chai';
 import * as path from 'path';
 import { ApplicationOptions } from '../../src/application/schema';
 import { ControllerOptions } from '../../src/controller/schema';
+import { ModuleOptions } from '../../src/module/schema';
 
 describe('Controller Factory', () => {
-  const options: ControllerOptions = {
-    name: 'name',
-  };
-  let tree: UnitTestTree;
-  before(() => {
-    const runner: SchematicTestRunner = new SchematicTestRunner(
-      '.',
-      path.join(process.cwd(), 'src/collection.json')
-    );
-    const appOptions: ApplicationOptions = {
-      directory: '',
+  describe('No module in controller generate directory', () => {
+    const options: ControllerOptions = {
+      name: 'name',
     };
-    const appTree: UnitTestTree = runner.runSchematic('application', appOptions, new VirtualTree());
-    tree = runner.runSchematic('controller', options, appTree);
-  });
-  it('should generate a new controller file', () => {
-    const files: string[] = tree.files;
-    expect(
-      files.find(
-        (filename) => filename === `/src/${ options.name }/${ options.name }.controller.ts`
-      )
-    ).to.not.be.undefined;
-  });
-  it('should generate the expected controller file content', () => {
-    expect(
-      tree
-        .readContent(path.join(
+    let tree: UnitTestTree;
+    before(() => {
+      const runner: SchematicTestRunner = new SchematicTestRunner(
+        '.',
+        path.join(process.cwd(), 'src/collection.json')
+      );
+      const appOptions: ApplicationOptions = {
+        directory: '',
+      };
+      const appTree: UnitTestTree = runner.runSchematic('application', appOptions, new VirtualTree());
+      tree = runner.runSchematic('controller', options, appTree);
+    });
+    it('should generate a new controller file', () => {
+      const files: string[] = tree.files;
+      expect(
+        files.find(
+          (filename) => filename === `/src/${ options.name }/${ options.name }.controller.ts`
+        )
+      ).to.not.be.undefined;
+    });
+    it('should generate the expected controller file content', () => {
+      expect(
+        tree
+          .readContent(path.join(
+            '/src',
+            options.name,
+            `${ options.name }.controller.ts`
+          ))
+      ).to.be.equal(
+        'import { Controller } from \'@nestjs/common\';\n' +
+        '\n' +
+        '@Controller()\n' +
+        'export class NameController {}\n'
+      );
+    });
+    it('should import the new controller in the application module', () => {
+      expect(
+        tree.readContent(path.join(
           '/src',
-          options.name,
-          `${ options.name }.controller.ts`
+          'app.module.ts'
         ))
-    ).to.be.equal(
-      'import { Controller } from \'@nestjs/common\';\n' +
-      '\n' +
-      '@Controller()\n' +
-      'export class NameController {}\n'
-    );
+      ).to.be.equal(
+        'import { Module } from \'@nestjs/common\';\n' +
+        'import { AppController } from \'./app.controller\';\n' +
+        'import { NameController } from \'./name/name.controller\';\n' +
+        '\n' +
+        '@Module({\n' +
+        '  imports: [],\n' +
+        '  controllers: [\n' +
+        '    AppController,\n' +
+        '    NameController\n' +
+        '  ],\n' +
+        '  components: []\n' +
+        '})\n' +
+        'export class ApplicationModule {}\n'
+      );
+    });
   });
-  it('should import the new controller in the application module', () => {
-    expect(
-      tree.readContent(path.join(
-        '/src',
-        'app.module.ts'
-      ))
-    ).to.be.equal(
-      'import { Module } from \'@nestjs/common\';\n' +
-      'import { AppController } from \'./app.controller\';\n' +
-      'import { NameController } from \'./name/name.controller\';\n' +
-      '\n' +
-      '@Module({\n' +
-      '  imports: [],\n' +
-      '  controllers: [\n' +
-      '    AppController,\n' +
-      '    NameController\n' +
-      '  ],\n' +
-      '  components: []\n' +
-      '})\n' +
-      'export class ApplicationModule {}\n'
-    );
+  describe('Generated module before controller generation', () => {
+    const options: ControllerOptions = {
+      name: 'name',
+    };
+    let tree: UnitTestTree;
+    before(() => {
+      const runner: SchematicTestRunner = new SchematicTestRunner(
+        '.',
+        path.join(process.cwd(), 'src/collection.json')
+      );
+      const appOptions: ApplicationOptions = {
+        directory: '',
+      };
+      let root: UnitTestTree = runner.runSchematic('application', appOptions, new VirtualTree());
+      const moduleOptions: ModuleOptions = {
+        name: options.name
+      };
+      root = runner.runSchematic('module', moduleOptions, root);
+      tree = runner.runSchematic('controller', options, root);
+    });
+    it('should import the new controller in the generated directory module', () => {
+      expect(tree.readContent(`/src/${options.name}/${options.name}.module.ts`))
+        .to.be.equal(
+        'import { Module } from \'@nestjs/common\';\n' +
+        'import { NameController } from \'./name.controller\';\n' +
+        '\n' +
+        '@Module({\n' +
+        '  controllers: [\n' +
+        '    NameController\n' +
+        '  ],\n' +
+        '})\n' +
+        'export class NameModule {}\n'
+      );
+    });
   });
 });
