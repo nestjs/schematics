e0762384a1ddbec3eac2f5afa1dc34c7873d5c67
thomrick <tricart@xebia.fr>
2018-03-24 16:28:29 +0100

    #nest-v5 : use module find utils to insert generated controller into the nearest module

--- src/controller/factory.ts
+++ src/controller/factory.ts
@@ -1,10 +1,11 @@
-import { strings } from '@angular-devkit/core';
+import { normalize, Path, relative, strings } from '@angular-devkit/core';
 import { classify } from '@angular-devkit/core/src/utils/strings';
 import { apply, chain, mergeWith, move, Rule, template, Tree, url } from '@angular-devkit/schematics';
 import { ModuleOptions } from '../module/schema';
 import { ModuleImportUtils } from '../utils/module-import.utils';
 import { ModuleMetadataUtils } from '../utils/module-metadata.utils';
 import { ControllerOptions } from './schema';
+import { ModuleFindUtils } from '../utils/module-find.utils';
 
 export function main(options: ControllerOptions): Rule {
   return chain([
@@ -27,12 +28,15 @@ function generate(options: ControllerOptions) {
 
 function addDeclarationToModule(options: ModuleOptions): Rule {
   return (tree: Tree) => {
+    const generatedDirectoryPath: string = normalize(`/src/${ options.name }`);
+    const moduleToInsertPath: string = ModuleFindUtils.find(tree, generatedDirectoryPath);
+    let relativePath: string = relative(moduleToInsertPath as Path, generatedDirectoryPath as Path);
+    relativePath = `${ relativePath.substring(1, relativePath.length) }/${ options.name }.controller`;
+    let content = tree.read(moduleToInsertPath).toString();
     const symbol: string = `${ classify(options.name) }Controller`;
-    const relativePath: string = `./${ options.name }/${ options.name }.controller`;
-    let content = tree.read('/src/app.module.ts').toString();
     content = ModuleImportUtils.insert(content, symbol, relativePath);
     content = ModuleMetadataUtils.insert(content, 'controllers', symbol);
-    tree.overwrite('/src/app.module.ts', content);
+    tree.overwrite(moduleToInsertPath, content);
     return tree;
   };
 }
