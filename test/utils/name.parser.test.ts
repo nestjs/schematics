82b09793ac9fbe4a5df4aba9fb48c3a09ed98321
thomrick <tricart@xebia.fr>
2018-03-25 12:02:53 +0200

    #nest-v5 : review name parser to be same as angular cli schematics

--- test/utils/name.parser.test.ts
+++ test/utils/name.parser.test.ts
@@ -4,48 +4,47 @@ import { Location, NameParser, ParseOptions } from '../../src/utils/name.parser'
 describe('Name Parser', () => {
   let parser: NameParser;
   before(() => parser = new NameParser());
-  it('should handle just the name', () => {
-    const options: ParseOptions = {
-      name: 'foo',
-      path: 'src'
-    };
-    const location: Location = parser.parse(options);
-    expect(location.name).to.be.equal('foo');
-    expect(location.path).to.be.equal('/src');
-  });
   it('should handle no path', () => {
     const options: ParseOptions = {
-      name: 'foo',
-      path: ''
+      name: 'foo'
     };
     const location: Location = parser.parse(options);
     expect(location.name).to.be.equal('foo');
     expect(location.path).to.be.equal('/');
   });
-  it('should handle name has a path (sub-dir)', () => {
+  it('should handle just the name', () => {
     const options: ParseOptions = {
-      name: 'bar/foo',
-      path: 'src/app'
+      name: 'foo',
+      path: 'baz'
     };
     const location: Location = parser.parse(options);
     expect(location.name).to.be.equal('foo');
-    expect(location.path).to.be.equal('/src/app/bar');
+    expect(location.path).to.be.equal('/baz');
+  });
+  it('should handle name has a path (sub-dir)', () => {
+    const options: ParseOptions = {
+      name: 'bar/foo',
+      path: 'baz'
+    };
+    const location: Location = parser.parse(options);
+    expect(location.name).to.be.equal('foo');
+    expect(location.path).to.be.equal('/baz/bar');
   });
 
   it('should handle name has a higher path', () => {
     const options: ParseOptions = {
       name: '../foo',
-      path: 'src/app'
+      path: 'bar/baz'
     };
     const location: Location = parser.parse(options);
     expect(location.name).to.be.equal('foo');
-    expect(location.path).to.be.equal('/src');
+    expect(location.path).to.be.equal('/bar');
   });
 
   it('should handle name has a higher path above root', () => {
     const options: ParseOptions = {
       name: '../../../foo',
-      path: 'src/app'
+      path: 'baz'
     };
     expect(() => parser.parse(options)).to.throw();
   });
