36827964aa17f9face69bce52a67caf25b9996a6
thomrick <tricart@xebia.fr>
2018-03-25 12:02:53 +0200

    #nest-v4 : review name parser to be same as angular cli schematics

--- src/utils/name.parser.ts
+++ src/utils/name.parser.ts
@@ -2,7 +2,7 @@ import { basename, dirname, normalize, Path } from '@angular-devkit/core';
 
 export interface ParseOptions {
   name: string;
-  path: string;
+  path?: string;
 }
 
 export interface Location {
@@ -14,11 +14,13 @@ export class NameParser {
   constructor() {}
 
   public parse(options: ParseOptions): Location {
-    const nameWithoutPath = basename(options.name as Path);
-    const namePath = dirname((options.path + '/' + options.name) as Path);
+    const nameWithoutPath: string = basename(options.name as Path);
+    const namePath: string = dirname(
+      (options.path === undefined ? '' : options.path).concat('/').concat(options.name) as Path)
+    ;
     return {
       name: nameWithoutPath,
-      path: normalize('/' + namePath),
+      path: normalize('/'.concat(namePath)),
     };
   }
 }
