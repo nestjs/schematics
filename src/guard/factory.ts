1f6882d2ec041fdc568ff8e413aa0c2282c544fa
thomrick <tricart@xebia.fr>
2018-03-31 14:21:39 +0200

    #rc-1 : review guard schematic factory to follow module service and controller generation

--- src/guard/factory.ts
+++ src/guard/factory.ts
@@ -1,17 +1,30 @@
-import { strings } from '@angular-devkit/core';
+import { join, normalize, Path, strings } from '@angular-devkit/core';
 import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
+import { Location, NameParser } from '../utils/name.parser';
 import { GuardOptions } from './schema';
 
 export function main(options: GuardOptions): Rule {
-  return mergeWith(
-    apply(
-      url('./files'), [
-        template({
-          ...strings,
-          ...options
-        }),
-        move('/src')
-      ]
-    )
+  options = transform(options);
+  return mergeWith(generate(options));
+}
+
+function transform(source: GuardOptions): GuardOptions{
+  let target: GuardOptions = Object.assign({}, source);
+  target.path = target.path !== undefined ? join(normalize('src'), target.path) : normalize('src');
+  const location: Location = new NameParser().parse(target);
+  target.name = location.name;
+  target.path = location.path;
+  return target;
+}
+
+function generate(options: GuardOptions) {
+  return apply(
+    url('./files'), [
+      template({
+        ...strings,
+        ...options
+      }),
+      move(join(options.path as Path, options.name))
+    ]
   );
 }
--- test/guard/factory.test.ts
+++ test/guard/factory.test.ts
@@ -5,39 +5,52 @@ import * as path from 'path';
 import { GuardOptions } from '../../src/guard/schema';
 
 describe('Guard Factory', () => {
-  const options: GuardOptions = {
-    name: 'name',
-  };
-  let runner: SchematicTestRunner;
-  before(() => {
-    runner = new SchematicTestRunner(
-      '.',
-      path.join(process.cwd(), 'src/collection.json')
-    );
-  });
-  it('should generate a new guard file', () => {
-    const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
-    const files: string[] = tree.files;
-    expect(
-      files.find((filename) =>
-        filename === `/src/${ options.name }.guard.ts`
-      )
-    ).to.not.be.undefined;
-  });
-  it('should generate the expected guard file content', () => {
-    const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
-    expect(
-      tree.readContent(`/src/${ options.name }.guard.ts`)
-    ).to.be.equal(
-      'import { Guard, CanActivate, ExecutionContext } from \'@nestjs/common\';\n' +
-      'import { Observable } from \'rxjs/Observable\';\n' +
-      '\n' +
-      '@Guard()\n' +
-      'export class NameGuard implements CanActivate {\n' +
-      '  canActivate(dataOrRequest, context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {\n' +
-      '    return true;\n' +
-      '  }\n' +
-      '}\n'
-    );
+  describe('Schematic definition', () => {
+    context('Manage name only', () => {
+      it('should generate a new guard file', () => {
+        const options: GuardOptions = {
+          name: 'foo'
+        };
+        const runner: SchematicTestRunner = new SchematicTestRunner(
+          '.',
+          path.join(process.cwd(), 'src/collection.json')
+        );
+        const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
+        const files: string[] = tree.files;
+        expect(files.find((filename) => filename === '/src/foo/foo.guard.ts'))
+          .to.not.be.undefined;
+      });
+    });
+    context('Manage name as a path', () => {
+      it('should generate a new guard file', () => {
+        const options: GuardOptions = {
+          name: 'bar/foo'
+        };
+        const runner: SchematicTestRunner = new SchematicTestRunner(
+          '.',
+          path.join(process.cwd(), 'src/collection.json')
+        );
+        const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
+        const files: string[] = tree.files;
+        expect(files.find((filename) => filename === '/src/bar/foo/foo.guard.ts'))
+          .to.not.be.undefined;
+      });
+    });
+    context('Manage name and path', () => {
+      it('should generate a new guard file', () => {
+        const options: GuardOptions = {
+          name: 'foo',
+          path: 'bar'
+        };
+        const runner: SchematicTestRunner = new SchematicTestRunner(
+          '.',
+          path.join(process.cwd(), 'src/collection.json')
+        );
+        const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
+        const files: string[] = tree.files;
+        expect(files.find((filename) => filename === '/src/bar/foo/foo.guard.ts'))
+          .to.not.be.undefined;
+      });
+    });
   });
 });
