7b3667053a946c5c083a4e02241ee58882fca9fd
thomrick <tricart@xebia.fr>
2018-03-31 20:08:10 +0200

    #rc3 : refactor guard schematic factory unit tests

--- test/application/factory.test.ts
+++ test/application/factory.test.ts
@@ -16,22 +16,21 @@ describe('Application Factory', () => {
     );
     tree = runner.runSchematic('application', options, new VirtualTree());
   });
-  it('should generate Nest application starter project structure',
-    () => {
-      const files: string[] = tree.files;
-      expect(files).to.be.deep.equal([
-        '/name/.prettierrc',
-        '/name/README.md',
-        '/name/nodemon.json',
-        '/name/package.json',
-        '/name/src/app.controller.spec.ts',
-        '/name/src/app.controller.ts',
-        '/name/src/app.module.ts',
-        '/name/src/main.ts',
-        '/name/test/app.e2e-spec.ts',
-        '/name/test/jest-e2e.json',
-        '/name/tsconfig.json',
-        '/name/tslint.json'
-      ]);
-    });
+  it('should generate Nest application starter project structure', () => {
+    const files: string[] = tree.files;
+    expect(files).to.be.deep.equal([
+      '/name/.prettierrc',
+      '/name/README.md',
+      '/name/nodemon.json',
+      '/name/package.json',
+      '/name/src/app.controller.spec.ts',
+      '/name/src/app.controller.ts',
+      '/name/src/app.module.ts',
+      '/name/src/main.ts',
+      '/name/test/app.e2e-spec.ts',
+      '/name/test/jest-e2e.json',
+      '/name/tsconfig.json',
+      '/name/tslint.json'
+    ]);
+  });
 });
--- test/guard/factory.test.ts
+++ test/guard/factory.test.ts
@@ -4,25 +4,15 @@ import { expect } from 'chai';
 import * as path from 'path';
 import { GuardOptions } from '../../src/guard/schema';
 import { ApplicationOptions } from '../../src/application/schema';
+import { ExceptionOptions } from '../../src/exception/schema';
 
 describe('Guard Factory', () => {
-  let tree: UnitTestTree;
-  let runner: SchematicTestRunner;
-  before(() => {
-    runner = new SchematicTestRunner(
-      '.',
-      path.join(process.cwd(), 'src/collection.json')
-    );
-    const options: ApplicationOptions = {
-      name: '',
-    };
-    tree = runner.runSchematic('application', options, new VirtualTree());
-  });
+  const runner: SchematicTestRunner = new SchematicTestRunner('.', path.join(process.cwd(), 'src/collection.json'));
   it('should manage name only', () => {
     const options: GuardOptions = {
       name: 'foo'
     };
-    tree = runner.runSchematic('guard', options, tree);
+    const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find((filename) =>
@@ -34,7 +24,7 @@ describe('Guard Factory', () => {
     const options: GuardOptions = {
       name: 'bar/foo'
     };
-    tree = runner.runSchematic('guard', options, tree);
+    const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find((filename) =>
@@ -47,7 +37,7 @@ describe('Guard Factory', () => {
       name: 'foo',
       path: 'baz'
     };
-    tree = runner.runSchematic('guard', options, tree);
+    const tree: UnitTestTree = runner.runSchematic('guard', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find((filename) =>
