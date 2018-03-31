05db4f60473570481ee1b4677508e7fa96e4bbcc
thomrick <tricart@xebia.fr>
2018-03-31 19:18:01 +0200

    #rc3 : refactor exception schematic factory

--- src/exception/factory.ts
+++ src/exception/factory.ts
@@ -1,17 +1,30 @@
-import { strings } from '@angular-devkit/core';
-import { apply, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
+import { join, normalize, Path, strings } from '@angular-devkit/core';
+import { apply, mergeWith, move, Rule, Source, template, url } from '@angular-devkit/schematics';
 import { ExceptionOptions } from './schema';
+import { Location, NameParser } from '../utils/name.parser';
 
 export function main(options: ExceptionOptions): Rule {
-  return mergeWith(
-    apply(
-      url('./files'), [
-        template({
-          ...strings,
-          ...options
-        }),
-        move('/src')
-      ]
-    )
+  options = transform(options);
+  return mergeWith(generate(options));
+}
+
+function transform(options: ExceptionOptions): ExceptionOptions {
+  const target = Object.assign({}, options);
+  target.path = target.path !== undefined ? join(normalize('src'), target.path) : normalize('src');
+  const location: Location = new NameParser().parse(target);
+  target.name = location.name;
+  target.path = location.path;
+  return target;
+}
+
+function generate(options: ExceptionOptions): Source {
+  return apply(
+    url('./files'), [
+      template({
+        ...strings,
+        ...options
+      }),
+      move(join(options.path as Path, options.name))
+    ]
   );
 }
--- test/exception/factory.test.ts
+++ test/exception/factory.test.ts
@@ -3,24 +3,55 @@ import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/te
 import { expect } from 'chai';
 import * as path from 'path';
 import { ExceptionOptions } from '../../src/exception/schema';
+import { ApplicationOptions } from '../../src/application/schema';
 
 describe('Exception Factory', () => {
-  const options: ExceptionOptions = {
-    name: 'name',
-  };
+  let tree: UnitTestTree;
   let runner: SchematicTestRunner;
   before(() => {
     runner = new SchematicTestRunner(
       '.',
       path.join(process.cwd(), 'src/collection.json')
     );
+    const options: ApplicationOptions = {
+      name: '',
+    };
+    tree = runner.runSchematic('application', options, new VirtualTree());
   });
-  it('should generate a new exception file', () => {
-    const tree: UnitTestTree = runner.runSchematic('exception', options, new VirtualTree());
+  it('should manage name only', () => {
+    const options: ExceptionOptions = {
+      name: 'foo'
+    };
+    tree = runner.runSchematic('exception', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find((filename) =>
-        filename === `/src/${ options.name }.exception.ts`
+        filename === `/src/foo/foo.exception.ts`
+      )
+    ).to.not.be.undefined;
+  });
+  it('should manage name as a path', () => {
+    const options: ExceptionOptions = {
+      name: 'bar/foo'
+    };
+    tree = runner.runSchematic('exception', options, new VirtualTree());
+    const files: string[] = tree.files;
+    expect(
+      files.find((filename) =>
+        filename === `/src/bar/foo/foo.exception.ts`
+      )
+    ).to.not.be.undefined;
+  });
+  it('should manage name and path', () => {
+    const options: ExceptionOptions = {
+      name: 'foo',
+      path: 'bar'
+    };
+    tree = runner.runSchematic('exception', options, new VirtualTree());
+    const files: string[] = tree.files;
+    expect(
+      files.find((filename) =>
+        filename === `/src/bar/foo/foo.exception.ts`
       )
     ).to.not.be.undefined;
   });
