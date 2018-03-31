801751dc0f80ee5528a19a61e797400f22ba5793
thomrick <tricart@xebia.fr>
2018-03-31 20:06:24 +0200

    #rc3 : refactor exception schematic factory unit tests

--- test/exception/factory.test.ts
+++ test/exception/factory.test.ts
@@ -6,23 +6,12 @@ import { ExceptionOptions } from '../../src/exception/schema';
 import { ApplicationOptions } from '../../src/application/schema';
 
 describe('Exception Factory', () => {
-  let tree: UnitTestTree;
-  let runner: SchematicTestRunner;
-  before(() => {
-    runner = new SchematicTestRunner(
-      '.',
-      path.join(process.cwd(), 'src/collection.json')
-    );
-    const options: ApplicationOptions = {
-      name: '',
-    };
-    tree = runner.runSchematic('application', options, new VirtualTree());
-  });
+  const runner: SchematicTestRunner = new SchematicTestRunner('.', path.join(process.cwd(), 'src/collection.json'));
   it('should manage name only', () => {
     const options: ExceptionOptions = {
       name: 'foo'
     };
-    tree = runner.runSchematic('exception', options, tree);
+    const tree: UnitTestTree = runner.runSchematic('exception', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find((filename) =>
@@ -34,7 +23,7 @@ describe('Exception Factory', () => {
     const options: ExceptionOptions = {
       name: 'bar/foo'
     };
-    tree = runner.runSchematic('exception', options, tree);
+    const tree: UnitTestTree = runner.runSchematic('exception', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find((filename) =>
@@ -47,7 +36,7 @@ describe('Exception Factory', () => {
       name: 'foo',
       path: 'baz'
     };
-    tree = runner.runSchematic('exception', options, tree);
+    const tree: UnitTestTree = runner.runSchematic('exception', options, new VirtualTree());
     const files: string[] = tree.files;
     expect(
       files.find((filename) =>
