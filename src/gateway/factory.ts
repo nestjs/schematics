aa25ec50a34ead6b50f1be837231e095e115b25a
thomrick <tricart@xebia.fr>
2018-04-04 23:10:18 +0200

    #nest-v4-rc4 : manage gateway

---
+++ src/gateway/factory.ts
@@ -0,0 +1,30 @@
+import { join, normalize, Path, strings } from '@angular-devkit/core';
+import { apply, mergeWith, move, Rule, Source, template, url } from '@angular-devkit/schematics';
+import { Location, NameParser } from '../utils/name.parser';
+import { GatewayOptions } from './schema';
+
+export function main(options: GatewayOptions): Rule {
+  options = transform(options);
+  return mergeWith(generate(options));
+}
+
+function transform(options: GatewayOptions): GatewayOptions {
+  const target: GatewayOptions = Object.assign({}, options);
+  target.path = target.path !== undefined ? join(normalize('src'), target.path) : normalize('src');
+  const location: Location = new NameParser().parse(target);
+  target.name = strings.dasherize(location.name);
+  target.path = strings.dasherize(location.path);
+  return target;
+}
+
+function generate(options: GatewayOptions): Source {
+  return apply(
+    url('./files'), [
+      template({
+        ...strings,
+        ...options
+      }),
+      move(join(options.path as Path, options.name))
+    ]
+  );
+}
