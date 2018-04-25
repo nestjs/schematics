e6499f7c12cedef22eec5dcf1dde6a04ba2ea9d0
thomrick <tricart@xebia.fr>
2018-04-25 16:24:00 +0200

    feature(javascript) : manage javascript exception schematics

--- src/exception/factory.ts
+++ src/exception/factory.ts
@@ -14,12 +14,13 @@ function transform(options: ExceptionOptions): ExceptionOptions {
   const location: Location = new NameParser().parse(target);
   target.name = strings.dasherize(location.name);
   target.path = strings.dasherize(location.path);
+  target.language = target.language ? target.language : 'ts';
   return target;
 }
 
 function generate(options: ExceptionOptions): Source {
   return apply(
-    url('./files'), [
+    url(join('files' as Path, options.language)), [
       template({
         ...strings,
         ...options
