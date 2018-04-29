1705b6dd74fc296430e4b41199b5e6e8bf086b01
thomrick <tricart@xebia.fr>
2018-04-29 14:53:17 +0200

    fix(5.3.0) : fix lint errors

--- src/utils/metadata.manager.ts
+++ src/utils/metadata.manager.ts
@@ -1,4 +1,20 @@
-import { SourceFile, Node, createSourceFile, ScriptTarget, Identifier, SyntaxKind, PropertyAccessExpression, Decorator, CallExpression, ObjectLiteralExpression, ObjectLiteralElement, PropertyAssignment, StringLiteral, ArrayLiteralExpression, NodeArray, Expression } from 'typescript';
+import {
+  ArrayLiteralExpression,
+  CallExpression,
+  createSourceFile,
+  Decorator,
+  Expression,
+  Identifier,
+  Node,
+  NodeArray,
+  ObjectLiteralElement,
+  ObjectLiteralExpression,
+  PropertyAssignment,
+  ScriptTarget,
+  SourceFile,
+  StringLiteral,
+  SyntaxKind
+} from 'typescript';
 
 export class MetadataManager {
   constructor(private content: string) {}
@@ -7,23 +23,22 @@ export class MetadataManager {
     const source: SourceFile = createSourceFile('filename.ts', this.content, ScriptTarget.ES2017);
     const decoratorNodes: Node[] = this.getDecoratorMetadata(source, '@Module');
     let node: Node = decoratorNodes[0];
-    
     const matchingProperties: ObjectLiteralElement[] = (node as ObjectLiteralExpression).properties
-      .filter(prop => prop.kind == SyntaxKind.PropertyAssignment)
+      .filter((prop) => prop.kind === SyntaxKind.PropertyAssignment)
       .filter((prop: PropertyAssignment) => {
         const name = prop.name;
         switch (name.kind) {
           case SyntaxKind.Identifier:
-            return (name as Identifier).getText(source) == metadata;
+            return (name as Identifier).getText(source) === metadata;
           case SyntaxKind.StringLiteral:
-            return (name as StringLiteral).text == metadata;
+            return (name as StringLiteral).text === metadata;
+          default:
+            return false;
         }
-        return false;
       });
-
-    if (matchingProperties.length == 0) {
+    if (matchingProperties.length === 0) {
       const expr = node as ObjectLiteralExpression;
-      if (expr.properties.length == 0) {
+      if (expr.properties.length === 0) {
         return this.insertMetadataToEmptyModuleDecorator(expr, metadata, symbol);
       } else {
         return this.insertNewMetadataToDecorator(expr, source, metadata, symbol);
@@ -35,16 +50,16 @@ export class MetadataManager {
 
   private getDecoratorMetadata(source: SourceFile, identifier: string): Node[] {
     return this.getSourceNodes(source)
-      .filter((node) => 
-        node.kind == SyntaxKind.Decorator && (node as Decorator).expression.kind == SyntaxKind.CallExpression
+      .filter((node) =>
+        node.kind === SyntaxKind.Decorator && (node as Decorator).expression.kind === SyntaxKind.CallExpression
       )
-      .map((node) => 
+      .map((node) =>
         (node as Decorator).expression as CallExpression
       )
-      .filter((expr) => 
-        expr.arguments[0] && expr.arguments[0].kind == SyntaxKind.ObjectLiteralExpression
+      .filter((expr) =>
+        expr.arguments[0] && expr.arguments[0].kind === SyntaxKind.ObjectLiteralExpression
       )
-      .map((expr) => 
+      .map((expr) =>
         expr.arguments[0] as ObjectLiteralExpression
       );
   }
@@ -64,7 +79,9 @@ export class MetadataManager {
     return result;
   }
 
-  private insertMetadataToEmptyModuleDecorator(expr: ObjectLiteralExpression, metadata: string, symbol: string): string {
+  private insertMetadataToEmptyModuleDecorator(
+    expr: ObjectLiteralExpression, metadata: string, symbol: string
+  ): string {
     const position = expr.getEnd() - 1;
     const toInsert = `  ${ metadata }: [${ symbol }]`;
     return this.content.split('').reduce((content, char, index) => {
@@ -76,10 +93,11 @@ export class MetadataManager {
     }, '');
   }
 
-  private insertNewMetadataToDecorator(expr: ObjectLiteralExpression, source: SourceFile, metadata: string, symbol: string): string {
+  private insertNewMetadataToDecorator(
+    expr: ObjectLiteralExpression, source: SourceFile, metadata: string, symbol: string
+  ): string {
     const node = expr.properties[expr.properties.length - 1];
     const position = node.getEnd();
-    // Get the indentation of the last element, if any.
     const text = node.getFullText(source);
     const matches = text.match(/^\r?\n\s*/);
     let toInsert: string;
@@ -97,29 +115,28 @@ export class MetadataManager {
     }, '');
   }
 
-  private insertSymbolToMetadata(source: SourceFile, matchingProperties: ObjectLiteralElement[], symbol: string): string {
+  private insertSymbolToMetadata(
+    source: SourceFile, matchingProperties: ObjectLiteralElement[], symbol: string
+  ): string {
     const assignment = matchingProperties[0] as PropertyAssignment;
-
     let node: Node | NodeArray<Expression>;
     const arrLiteral = assignment.initializer as ArrayLiteralExpression;
-    if (arrLiteral.elements.length == 0) {
+    if (arrLiteral.elements.length === 0) {
       node = arrLiteral;
     } else {
       node = arrLiteral.elements;
     }
-
     if (Array.isArray(node)) {
-      const nodeArray = node as {} as Array<Node>;
-      const symbolsArray = nodeArray.map((node) => node.getText(source));
+      const nodeArray = node as {} as Node[];
+      const symbolsArray = nodeArray.map((childNode) => childNode.getText(source));
       if (symbolsArray.includes(symbol)) {
         return this.content;
       }
       node = node[ node.length - 1 ];
     }
-    
     let toInsert: string;
     let position = (node as Node).getEnd();
-    if ((node as Node).kind == SyntaxKind.ArrayLiteralExpression) {
+    if ((node as Node).kind === SyntaxKind.ArrayLiteralExpression) {
       position--;
       toInsert = `${symbol}`;
     } else {
