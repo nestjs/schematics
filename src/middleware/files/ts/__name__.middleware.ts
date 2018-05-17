3055ccb34b6e83a1e0228355f41b11951831d694
Maxi Nivoli <m_nivoli@hotmail.com>
2018-05-17 14:09:59 -0300

    fix() : fix middleware ts file import and middleware test

--- src/middleware/files/ts/__name__.middleware.ts
+++ src/middleware/files/ts/__name__.middleware.ts
@@ -1,8 +1,8 @@
-import { Injectable, NestMiddleware, FunctionMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, MiddlewareFunction } from '@nestjs/common';
 
 @Injectable()
 export class <%= classify(name) %>Middleware implements NestMiddleware {
-  resolve(context: string): FunctionMiddleware {
+  resolve(context: string): MiddlewareFunction {
     return (req, res, next) => {
       next();
     };
