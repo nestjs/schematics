d4c7e34fba9a482bd6dc6b9ab194b1d295af766d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-05-26 23:04:03 +0200

    fix: remove redundant generics

--- src/pipe/files/ts/__name__.pipe.ts
+++ src/pipe/files/ts/__name__.pipe.ts
@@ -1,7 +1,7 @@
 import { ArgumentMetadata, Pipe, PipeTransform } from '@nestjs/common';
 
 @Pipe()
-export class <%= classify(name) %>Pipe implements PipeTransform<any> {
+export class <%= classify(name) %>Pipe implements PipeTransform {
   async transform(value: any, metadata: ArgumentMetadata) {
     return value;
   }
