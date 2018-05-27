d7ad71d5eb6060cf585874ec44686aee5cdccaf9
thomrick <tricart@xebia.fr>
2018-05-27 17:59:51 +0200

    enhancement() : fix unit tests after rebase on sprint-2

--- test/utils/module.finder.test.ts
+++ test/utils/module.finder.test.ts
@@ -14,18 +14,6 @@ describe('Module Finder', () => {
     expect(finder.find(options))
       .toEqual(normalize('/src/app.module.ts'));
   });
-  it('should return the generated name module path', () => {
-    const tree = new EmptyTree();
-    tree.create('/src/app.module.ts', 'app module content');
-    tree.create('/src/foo/foo.module.ts', 'foo module content');
-    const finder = new ModuleFinder(tree);
-    const options: FindOptions = {
-      name: 'foo',
-      path: normalize('/src')
-    };
-    expect(finder.find(options))
-      .toEqual(normalize('/src/foo/foo.module.ts'));
-  });
   it('should return the intermediate module path', () => {
     const tree = new EmptyTree();
     tree.create('/src/app.module.ts', 'app module content');
