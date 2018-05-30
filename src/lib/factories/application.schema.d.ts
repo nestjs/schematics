86b2ba62b20328090b1608cd29756deeec2eeeb4
thomrick <tricart@xebia.fr>
2018-05-30 10:25:44 +0200

    enhancement(build) : refactor project structure to separate templates from factories

---
+++ src/lib/factories/application.schema.d.ts
@@ -0,0 +1,22 @@
+export interface ApplicationOptions {
+  /**
+   * The Nest application author.
+   */
+  author?: string;
+  /**
+   * The Nest application description.
+   */
+  description?: string;
+  /**
+   * The Nest application name.
+   */
+  name: string;
+  /**
+   * The Nest application version.
+   */
+  version?: string;
+  /**
+   * The Nest configuration language.
+   */
+  language?: string;
+}
---
+++ src/templates/js/application/src/app.controller.js
@@ -0,0 +1,15 @@
+import { Controller, Dependencies, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+@Dependencies(AppService)
+export class AppController {
+  constructor(appService) {
+    this.appService = appService;
+  }
+
+  @Get()
+  root() {
+    return this.appService.root();
+  }
+}
