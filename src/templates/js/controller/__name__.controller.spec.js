9e7777c3db4b2c047f52d73902d051bda7295ae9
thomrick <tricart@xebia.fr>
2018-05-30 16:12:00 +0200

    feature(tests) : generate provider spec

--- src/templates/js/controller/__name__.controller.spec.js
+++ src/templates/js/controller/__name__.controller.spec.js
@@ -1,15 +1,15 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { <%= classify(name) %>Controller } from './<%= name %>.controller';
 
 describe('<%= classify(name) %> Controller', () => {
-  let module: TestingModule;
+  let module;
   beforeAll(async () => {
     module = await Test.createTestingModule({
       controllers: [<%= classify(name) %>Controller],
     }).compile();
   });
   it('should be defined', () => {
-    const controller: <%= classify(name) %>Controller = module.get<<%= classify(name) %>Controller>(<%= classify(name) %>Controller);
-    expect(controller).toBeDefined();
+    const controller = module.get(<%= classify(name) %>Controller);
+    expect(controller).not.toBeUndefined();
   });
 });
---
+++ src/templates/js/provider/__name__.spec.js
@@ -0,0 +1,15 @@
+import { Test } from '@nestjs/testing';
+import { <%= classify(name) %> } from './<%= name %>';
+
+describe('<%= classify(name) %>', () => {
+  let provider;
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [<%= classify(name) %>],
+    }).compile();
+    provider = module.get(<%= classify(name) %>);
+  });
+  it('should be defined', () => {
+    expect(provider).toBeDefined();
+  });
+});
