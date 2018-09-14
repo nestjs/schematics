9676111384ed0cf5f606fddc87fadb87e1ba82d6
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-09-14 13:28:57 +0200

    feature: use nest-cli.json instead of nestcli

--- test/lib/factories/configuration.factory.test.ts
+++ test/lib/factories/configuration.factory.test.ts
@@ -1,46 +1,88 @@
 import { VirtualTree } from '@angular-devkit/schematics';
-import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { ConfigurationOptions } from '../../../src/lib/factories/configuration.schema';
 
 describe('Configuration Factory', () => {
-  const runner: SchematicTestRunner = new SchematicTestRunner('.', path.join(process.cwd(), 'src/collection.json'));
+  const runner: SchematicTestRunner = new SchematicTestRunner(
+    '.',
+    path.join(process.cwd(), 'src/collection.json'),
+  );
   it('should manage a default configuation', () => {
     const options: ConfigurationOptions = {
-      project: 'project'
+      project: 'project',
     };
-    const tree: UnitTestTree = runner.runSchematic('configuration', options, new VirtualTree());
+    const tree: UnitTestTree = runner.runSchematic(
+      'configuration',
+      options,
+      new VirtualTree(),
+    );
     const files: string[] = tree.files;
-    expect(files.find((filename) => filename === '/project/.nestcli.json')).toBeDefined();
-    expect(tree.readContent('/project/.nestcli.json')).toEqual(JSON.stringify({
-      language: 'ts',
-      collection: '@nestjs/schematics'
-    }, null, 2));
+    expect(
+      files.find(filename => filename === '/project/.nest-cli.json'),
+    ).toBeDefined();
+    expect(tree.readContent('/project/.nest-cli.json')).toEqual(
+      JSON.stringify(
+        {
+          language: 'ts',
+          collection: '@nestjs/schematics',
+        },
+        null,
+        2,
+      ),
+    );
   });
   it('should manage provided language input', () => {
     const options: ConfigurationOptions = {
       project: 'project',
-      language: 'js'
-    };
-    const tree: UnitTestTree = runner.runSchematic('configuration', options, new VirtualTree());
-    const files: string[] = tree.files;
-    expect(files.find((filename) => filename === '/project/.nestcli.json')).toBeDefined();
-    expect(tree.readContent('/project/.nestcli.json')).toEqual(JSON.stringify({
       language: 'js',
-      collection: '@nestjs/schematics'
-    }, null, 2));
+    };
+    const tree: UnitTestTree = runner.runSchematic(
+      'configuration',
+      options,
+      new VirtualTree(),
+    );
+    const files: string[] = tree.files;
+    expect(
+      files.find(filename => filename === '/project/.nest-cli.json'),
+    ).toBeDefined();
+    expect(tree.readContent('/project/.nest-cli.json')).toEqual(
+      JSON.stringify(
+        {
+          language: 'js',
+          collection: '@nestjs/schematics',
+        },
+        null,
+        2,
+      ),
+    );
   });
   it('should manage provided collection input', () => {
     const options: ConfigurationOptions = {
       project: 'project',
-      collection: 'foo'
+      collection: 'foo',
     };
-    const tree: UnitTestTree = runner.runSchematic('configuration', options, new VirtualTree());
+    const tree: UnitTestTree = runner.runSchematic(
+      'configuration',
+      options,
+      new VirtualTree(),
+    );
     const files: string[] = tree.files;
-    expect(files.find((filename) => filename === '/project/.nestcli.json')).not.toBeUndefined();
-    expect(tree.readContent('/project/.nestcli.json')).toEqual(JSON.stringify({
-      language: 'ts',
-      collection: 'foo'
-    }, null, 2));
+    expect(
+      files.find(filename => filename === '/project/.nest-cli.json'),
+    ).not.toBeUndefined();
+    expect(tree.readContent('/project/.nest-cli.json')).toEqual(
+      JSON.stringify(
+        {
+          language: 'ts',
+          collection: 'foo',
+        },
+        null,
+        2,
+      ),
+    );
   });
 });
