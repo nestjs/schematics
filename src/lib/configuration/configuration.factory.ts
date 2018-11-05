010e1b0e79c3a32732b0c7ed6f44a91b10474583
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-11-05 19:21:10 +0100

    format: fix linter issues

--- src/lib/configuration/configuration.factory.ts
+++ src/lib/configuration/configuration.factory.ts
@@ -1,7 +1,15 @@
-import { join, Path, strings  } from '@angular-devkit/core';
-import { apply, mergeWith, move, Rule, Source, template, url } from '@angular-devkit/schematics';
-import { ConfigurationOptions } from './configuration.schema';
+import { join, Path, strings } from '@angular-devkit/core';
+import {
+  apply,
+  mergeWith,
+  move,
+  Rule,
+  Source,
+  template,
+  url,
+} from '@angular-devkit/schematics';
 import { DEFAULT_LANGUAGE } from '../defaults';
+import { ConfigurationOptions } from './configuration.schema';
 
 export function main(options: ConfigurationOptions): Rule {
   return mergeWith(generate(transform(options)));
@@ -9,20 +17,19 @@ export function main(options: ConfigurationOptions): Rule {
 
 function transform(options: ConfigurationOptions): ConfigurationOptions {
   const target: ConfigurationOptions = Object.assign({}, options);
-  target.language = target.language !== undefined ? target.language : DEFAULT_LANGUAGE;
-  target.collection = target.collection !== undefined ? target.collection : '@nestjs/schematics';
+  target.language =
+    target.language !== undefined ? target.language : DEFAULT_LANGUAGE;
+  target.collection =
+    target.collection !== undefined ? target.collection : '@nestjs/schematics';
   return target;
 }
 
 function generate(options: ConfigurationOptions): Source {
-  return apply(
-    url(join('./files' as Path, options.language)),
-    [
-      template({
-        ...strings,
-        ...options
-      }),
-      move(options.project)
-    ]
-  );
+  return apply(url(join('./files' as Path, options.language)), [
+    template({
+      ...strings,
+      ...options,
+    }),
+    move(options.project),
+  ]);
 }
