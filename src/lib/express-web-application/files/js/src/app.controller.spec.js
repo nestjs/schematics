373c68e872ec4ce049981c698aa235b98e90c6e3
Thomas Ricart <thomasr2309@gmail.com>
2019-05-20 13:54:22 +0200

    feature(application-type) : add express web application schematics

---
+++ src/lib/express-web-application/files/js/src/app.controller.spec.js
@@ -0,0 +1,22 @@
+import { Test } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController;
+
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
