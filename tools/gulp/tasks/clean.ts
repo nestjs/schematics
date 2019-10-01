d4728670c8d950267f717ff62606e15fc43b85f6
John Biundo <johnfbiundo@gmail.com>
2019-10-01 07:43:39 -0700

    feat(API) adds schematics utils to public API

---
+++ tools/gulp/tasks/clean.ts
@@ -0,0 +1,33 @@
+import { src, task } from 'gulp';
+import * as clean from 'gulp-clean';
+import { dist, sources } from '../config';
+
+/**
+ * Cleans the build output assets from the src folders
+ */
+function cleanSrc() {
+  const files = sources.map(folder => [
+    `${folder}/**/*.js`,
+    `${folder}/**/*.d.ts`,
+    `${folder}/**/*.js.map`,
+    `${folder}/**/*.d.ts.map`,
+  ]);
+  return src(files.reduce((a, b) => a.concat(b), []), {
+    read: false,
+    ignore: ['**/files/**/*', '**/*.schema.d.ts', '**/workspace/**/*'],
+  }).pipe(clean());
+}
+
+/**
+ * Cleans the source assets from the dist folders
+ */
+function cleanDist() {
+  const files = dist.map(folder => [`${folder}/**/*.ts`]);
+  return src(files.reduce((a, b) => a.concat(b), []), {
+    read: false,
+    ignore: ['**/files/**/*'],
+  }).pipe(clean());
+}
+
+task('clean:src', cleanSrc);
+task('clean:dist', cleanDist);
