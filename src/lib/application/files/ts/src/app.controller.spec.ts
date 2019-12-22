91a735045d0532e5ac87f9b5bd0c158e2ee474ad
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-12-22 13:44:18 +0200

    Updated TS application files to use jest-runner-groups

--- src/lib/application/files/ts/src/app.controller.spec.ts
+++ src/lib/application/files/ts/src/app.controller.spec.ts
@@ -1,3 +1,9 @@
+/**
+ * Tests AppController class.
+ * 
+ * @group functional/App/AppController
+ */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
--- src/lib/application/files/ts/test/e2e/app.spec.ts
+++ src/lib/application/files/ts/test/e2e/app.spec.ts
@@ -1,7 +1,14 @@
+/**
+ * Tests app controller.
+ * 
+ * @group supertest
+ * @group e2e/App/AppController
+ */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from './../../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
