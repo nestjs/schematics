97f0a80274df617626959a1ed801a5f2ff13f41e
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-12-22 13:55:09 +0200

    Fixed path to the app.module file in the js version

--- src/lib/application/files/js/test/e2e/app.spec.js
+++ src/lib/application/files/js/test/e2e/app.spec.js
@@ -7,7 +7,7 @@
 
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import { AppModule } from '../../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app;
