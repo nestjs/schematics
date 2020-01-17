5ee6b4ab4375b3ca7d90942fe75d1ee1235360c9
Livio Brunnner <livio.brunner.lb1@gmail.com>
2020-01-18 00:19:14 +0100

    feat: Log server url in main file

--- src/lib/application/files/js/src/main.js
+++ src/lib/application/files/js/src/main.js
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const url = await app.getUrl();
+  // tslint:disable-next-line: no-console
+  console.log(`** NestJS Server is listening on ${url}`);
 }
 bootstrap();
