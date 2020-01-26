07a7234083da51753fe999e07104a3a86a06f5c3
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-01-26 11:55:12 +0100

    fix(): removeno use before define from rules (app schematic)

--- src/lib/application/files/ts/.eslintrc.js
+++ src/lib/application/files/ts/.eslintrc.js
@@ -20,6 +20,5 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
   },
 };
