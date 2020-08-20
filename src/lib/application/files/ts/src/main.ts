deea7a7dac3155b06ca75f332f1f02b8126003c3
nightwind <44315206+nightwind2020@users.noreply.github.com>
2020-08-20 21:48:36 +0800

    chore(): update template code to be consistent with documentation

--- src/lib/application/files/ts/src/main.ts
+++ src/lib/application/files/ts/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
 }
 bootstrap();
