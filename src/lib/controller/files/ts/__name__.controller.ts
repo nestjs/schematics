55c54c0831076bbb43639328ed1850211917cb4b
Oleksandr Hladin <alexgladin17@gmail.com>
2020-08-21 23:04:27 +0300

    feat(add-resources-generator): add resource condition to templates

--- src/lib/controller/files/ts/__name__.controller.ts
+++ src/lib/controller/files/ts/__name__.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Param } from '@nestjs/common';
 
 @Controller('<%= dasherize(name) %>')
-export class <%= classify(name) %>Controller {}
+export class <%= classify(name) %>Controller {
+  <% if (resource) { %>
+  @Post()
+  create() {
+    return 'This action creates a new item'
+  }
+
+  @Get()
+  findAll() {
+    return 'This action returns all items'
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return 'This action updates an item'
+  }
+
+  @Delete(':id')
+  deleteImage(@Param('id') id: string) {
+    return this.imagesService.deleteImage(id)
+  }
+  <% } %>
+}
