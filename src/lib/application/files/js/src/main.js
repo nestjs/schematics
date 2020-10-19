3c4e24523b47aa9c149ce709439ff3217da0959f
Ilya Radchenko <knownasilya@gmail.com>
2020-10-19 12:19:30 -0400

    fix: try/catch for js main bootstrap

--- src/lib/application/files/js/src/main.js
+++ src/lib/application/files/js/src/main.js
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  } catch(error) {
+    console.error(error);
+  }
 }
 bootstrap();
