896d45ecc131d6561b16d679bf7a0f9c235a8114
Abdullah DURSUN <adursuns@gmail.com>
2020-10-22 21:05:05 +0300

    Remove extra spaces in controller

--- src/lib/resource/resource.factory.test.ts
+++ src/lib/resource/resource.factory.test.ts
@@ -85,7 +85,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  
+
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
