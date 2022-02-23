82845f5c7690ae1f90f2a29d14ff1b15f2f337be
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-02-23 08:32:37 -0400

    fix: package's name for scoped packages

--- src/lib/application/application.factory.test.ts
+++ src/lib/application/application.factory.test.ts
@@ -67,7 +67,7 @@ describe('Application Factory', () => {
         name: 'project.foo.bar',
       });
     });
-    it('should manage name to dasherize', async () => {
+    it('should manage name to dasherize from camel case name', async () => {
       const options: ApplicationOptions = {
         name: 'awesomeProject',
       };
@@ -93,6 +93,94 @@ describe('Application Factory', () => {
 
       expect(JSON.parse(tree.readContent('/awesome-project/package.json'))).toMatchObject({
         name: 'awesome-project',
+      });
+    });
+    it('should manage basic name that has no scope name in it but starts with "@"', async () => {
+      const options: ApplicationOptions = {
+        name: '@/package',
+      };
+      const tree: UnitTestTree = await runner.runSchematicAsync('application', options).toPromise();
+      const files: string[] = tree.files;
+      expect(files).toEqual([
+        '/@/package/.eslintrc.js',
+        '/@/package/.gitignore',
+        '/@/package/.prettierrc',
+        '/@/package/README.md',
+        '/@/package/nest-cli.json',
+        '/@/package/package.json',
+        '/@/package/tsconfig.build.json',
+        '/@/package/tsconfig.json',
+        '/@/package/src/app.controller.spec.ts',
+        '/@/package/src/app.controller.ts',
+        '/@/package/src/app.module.ts',
+        '/@/package/src/app.service.ts',
+        '/@/package/src/main.ts',
+        '/@/package/test/app.e2e-spec.ts',
+        '/@/package/test/jest-e2e.json',
+      ]);
+
+      expect(JSON.parse(tree.readContent('/@/package/package.json'))).toMatchObject({
+        name: 'package',
+      });
+    });
+    describe('and it meant to be a scoped package', () => {
+      describe('that leads to a valid scope name', () => {
+        it('should manage basic name', async () => {
+          const options: ApplicationOptions = {
+            name: '@scope/package',
+          };
+          const tree: UnitTestTree = await runner.runSchematicAsync('application', options).toPromise();
+          const files: string[] = tree.files;
+          expect(files).toEqual([
+            '/@scope/package/.eslintrc.js',
+            '/@scope/package/.gitignore',
+            '/@scope/package/.prettierrc',
+            '/@scope/package/README.md',
+            '/@scope/package/nest-cli.json',
+            '/@scope/package/package.json',
+            '/@scope/package/tsconfig.build.json',
+            '/@scope/package/tsconfig.json',
+            '/@scope/package/src/app.controller.spec.ts',
+            '/@scope/package/src/app.controller.ts',
+            '/@scope/package/src/app.module.ts',
+            '/@scope/package/src/app.service.ts',
+            '/@scope/package/src/main.ts',
+            '/@scope/package/test/app.e2e-spec.ts',
+            '/@scope/package/test/jest-e2e.json',
+          ]);
+
+          expect(JSON.parse(tree.readContent('/@scope/package/package.json'))).toMatchObject({
+            name: '@scope/package',
+          });
+        });
+        it('should manage name with blank space right after the "@" symbol', async () => {
+          const options: ApplicationOptions = {
+            name: '@ /package',
+          };
+          const tree: UnitTestTree = await runner.runSchematicAsync('application', options).toPromise();
+          const files: string[] = tree.files;
+          expect(files).toEqual([
+            '/@-/package/.eslintrc.js',
+            '/@-/package/.gitignore',
+            '/@-/package/.prettierrc',
+            '/@-/package/README.md',
+            '/@-/package/nest-cli.json',
+            '/@-/package/package.json',
+            '/@-/package/tsconfig.build.json',
+            '/@-/package/tsconfig.json',
+            '/@-/package/src/app.controller.spec.ts',
+            '/@-/package/src/app.controller.ts',
+            '/@-/package/src/app.module.ts',
+            '/@-/package/src/app.service.ts',
+            '/@-/package/src/main.ts',
+            '/@-/package/test/app.e2e-spec.ts',
+            '/@-/package/test/jest-e2e.json',
+          ]);
+
+          expect(JSON.parse(tree.readContent('/@-/package/package.json'))).toMatchObject({
+            name: '@-/package',
+          });
+        });
       });
     });
   });
@@ -128,31 +216,31 @@ describe('Application Factory', () => {
   });
   it('should manage destination directory', async () => {
     const options: ApplicationOptions = {
-      name: '@scope/package',
-      directory: 'scope-package',
+      name: 'project',
+      directory: 'app',
     };
     const tree: UnitTestTree = await runner.runSchematicAsync('application', options).toPromise();
     const files: string[] = tree.files;
     expect(files).toEqual([
-      '/scope-package/.eslintrc.js',
-      '/scope-package/.gitignore',
-      '/scope-package/.prettierrc',
-      '/scope-package/README.md',
-      '/scope-package/nest-cli.json',
-      '/scope-package/package.json',
-      '/scope-package/tsconfig.build.json',
-      '/scope-package/tsconfig.json',
-      '/scope-package/src/app.controller.spec.ts',
-      '/scope-package/src/app.controller.ts',
-      '/scope-package/src/app.module.ts',
-      '/scope-package/src/app.service.ts',
-      '/scope-package/src/main.ts',
-      '/scope-package/test/app.e2e-spec.ts',
-      '/scope-package/test/jest-e2e.json',
+      '/app/.eslintrc.js',
+      '/app/.gitignore',
+      '/app/.prettierrc',
+      '/app/README.md',
+      '/app/nest-cli.json',
+      '/app/package.json',
+      '/app/tsconfig.build.json',
+      '/app/tsconfig.json',
+      '/app/src/app.controller.spec.ts',
+      '/app/src/app.controller.ts',
+      '/app/src/app.module.ts',
+      '/app/src/app.service.ts',
+      '/app/src/main.ts',
+      '/app/test/app.e2e-spec.ts',
+      '/app/test/jest-e2e.json',
     ]);
 
-    expect(JSON.parse(tree.readContent('/scope-package/package.json'))).toMatchObject({
-      name: 'package',
+    expect(JSON.parse(tree.readContent('/app/package.json'))).toMatchObject({
+      name: 'project',
     });
   });
 });
