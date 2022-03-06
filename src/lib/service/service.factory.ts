2745cdf5da50f1cedcd5da86fe92480c0718a127
Zlatin Stanimirov <zlatin.stanimirov@adeccogroup.com>
2022-03-06 14:34:51 +0200

    refactor(service.factory): keep underscores in path and file name

--- src/lib/service/service.factory.ts
+++ src/lib/service/service.factory.ts
@@ -15,6 +15,7 @@ import {
   url,
 } from '@angular-devkit/schematics';
 import { isNullOrUndefined } from 'util';
+import { normalizeToKebabOrSnakeCase } from '../../utils/formatting';
 import {
   DeclarationOptions,
   ModuleDeclarator,
@@ -46,8 +47,8 @@ function transform(source: ServiceOptions): ServiceOptions {
     throw new SchematicsException('Option (name) is required.');
   }
   const location: Location = new NameParser().parse(target);
-  target.name = strings.dasherize(location.name);
-  target.path = strings.dasherize(location.path);
+  target.name = normalizeToKebabOrSnakeCase(location.name);
+  target.path = normalizeToKebabOrSnakeCase(location.path);
   target.language = target.language !== undefined ? target.language : 'ts';
 
   target.path = target.flat
@@ -59,7 +60,7 @@ function transform(source: ServiceOptions): ServiceOptions {
 function generate(options: ServiceOptions) {
   return (context: SchematicContext) =>
     apply(url(join('./files' as Path, options.language)), [
-      options.spec ? noop() : filter(path => !path.endsWith('.spec.ts')),
+      options.spec ? noop() : filter((path) => !path.endsWith('.spec.ts')),
       template({
         ...strings,
         ...options,
