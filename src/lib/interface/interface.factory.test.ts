36cadec8d213123281889ffbd27beda2caa75d8e
Zlatin Stanimirov <zlatin.stanimirov@adeccogroup.com>
2022-03-06 12:55:23 +0200

    refactor(interface.factory): keep underscores in path and file name

--- src/lib/interface/interface.factory.test.ts
+++ src/lib/interface/interface.factory.test.ts
@@ -15,10 +15,12 @@ describe('Interface Factory', () => {
     const options: InterfaceOptions = {
       name: 'foo',
     };
-    const tree: UnitTestTree = await runner.runSchematicAsync('interface', options).toPromise();
+    const tree: UnitTestTree = await runner
+      .runSchematicAsync('interface', options)
+      .toPromise();
     const files: string[] = tree.files;
     expect(
-      files.find(filename => filename === '/foo.interface.ts'),
+      files.find((filename) => filename === '/foo.interface.ts'),
     ).toBeDefined();
     expect(tree.readContent('/foo.interface.ts')).toEqual(
       'export interface Foo {}\n',
@@ -29,10 +31,12 @@ describe('Interface Factory', () => {
     const options: InterfaceOptions = {
       name: 'bar/foo',
     };
-    const tree: UnitTestTree = await runner.runSchematicAsync('interface', options).toPromise();
+    const tree: UnitTestTree = await runner
+      .runSchematicAsync('interface', options)
+      .toPromise();
     const files: string[] = tree.files;
     expect(
-      files.find(filename => filename === '/bar/foo.interface.ts'),
+      files.find((filename) => filename === '/bar/foo.interface.ts'),
     ).toBeDefined();
     expect(tree.readContent('/bar/foo.interface.ts')).toEqual(
       'export interface Foo {}\n',
@@ -44,40 +48,62 @@ describe('Interface Factory', () => {
       name: 'foo',
       path: 'baz',
     };
-    const tree: UnitTestTree = await runner.runSchematicAsync('interface', options).toPromise();
+    const tree: UnitTestTree = await runner
+      .runSchematicAsync('interface', options)
+      .toPromise();
     const files: string[] = tree.files;
     expect(
-      files.find(filename => filename === '/baz/foo.interface.ts'),
+      files.find((filename) => filename === '/baz/foo.interface.ts'),
     ).toBeDefined();
     expect(tree.readContent('/baz/foo.interface.ts')).toEqual(
       'export interface Foo {}\n',
     );
   });
 
-  it('should manage name to dasherize', async () => {
+  it('should manage name to normalize', async () => {
     const options: InterfaceOptions = {
       name: 'fooBar',
     };
-    const tree: UnitTestTree = await runner.runSchematicAsync('interface', options).toPromise();
+    const tree: UnitTestTree = await runner
+      .runSchematicAsync('interface', options)
+      .toPromise();
     const files: string[] = tree.files;
     expect(
-      files.find(filename => filename === '/foo-bar.interface.ts'),
+      files.find((filename) => filename === '/foo-bar.interface.ts'),
     ).toBeDefined();
     expect(tree.readContent('/foo-bar.interface.ts')).toEqual(
       'export interface FooBar {}\n',
     );
   });
 
-  it('should manage path to dasherize', async () => {
+  it('should manage path to normalize', async () => {
     const options: InterfaceOptions = {
       name: 'barBaz/foo',
     };
-    const tree: UnitTestTree = await runner.runSchematicAsync('interface', options).toPromise();
+    const tree: UnitTestTree = await runner
+      .runSchematicAsync('interface', options)
+      .toPromise();
     const files: string[] = tree.files;
     expect(
-      files.find(filename => filename === '/bar-baz/foo.interface.ts'),
+      files.find((filename) => filename === '/bar-baz/foo.interface.ts'),
     ).toBeDefined();
     expect(tree.readContent('/bar-baz/foo.interface.ts')).toEqual(
+      'export interface Foo {}\n',
+    );
+  });
+
+  it('should keep underscores in path and file names', async () => {
+    const options: InterfaceOptions = {
+      name: '_bar/_foo',
+    };
+    const tree: UnitTestTree = await runner
+      .runSchematicAsync('interface', options)
+      .toPromise();
+    const files: string[] = tree.files;
+    expect(
+      files.find((filename) => filename === '/_bar/_foo.interface.ts'),
+    ).toBeDefined();
+    expect(tree.readContent('/_bar/_foo.interface.ts')).toEqual(
       'export interface Foo {}\n',
     );
   });
