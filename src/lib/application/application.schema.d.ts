83e63aaf3974e26ecb1d60f1f6ac45dc27734f58
pdobrowolski99 <45772586+pdobrowolski99@users.noreply.github.com>
2022-04-27 07:57:05 +0200

    Update note
    Co-authored-by: Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>

--- src/lib/application/application.schema.d.ts
+++ src/lib/application/application.schema.d.ts
@@ -2,6 +2,7 @@ export interface ApplicationOptions {
   /**
    * Nest application name.
    */
+  // NOTE: We could have a `number` here due to this issue: https://github.com/nestjs/nest-cli/issues/1519
   name: string | number;
   /**
    * Nest application author.
