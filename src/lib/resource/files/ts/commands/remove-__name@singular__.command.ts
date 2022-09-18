ec6da4fb3416f80de246e20dc83b23209b26a6f7
Rene van Spronsen <renespronsen@gmail.com>
2022-09-18 17:07:25 +0200

    feat(cqrs): remove unused dto import

--- src/lib/resource/files/ts/commands/remove-__name@singular__.command.ts
+++ src/lib/resource/files/ts/commands/remove-__name@singular__.command.ts
@@ -1,5 +1,3 @@
-import { Remove<%= singular(classify(name)) %>Dto } from '../dto/remove-<%= singular(name) %>.dto';
-
 export class Remove<%= singular(classify(name)) %>Command {
   constructor(
     public readonly id: number,
