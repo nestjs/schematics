9454bc7cc55c4d5bdce054364a920d3c10630da1
Rene van Spronsen <renespronsen@gmail.com>
2022-09-18 17:06:50 +0200

    feat(cqrs): move control flow away from consumer

--- src/lib/resource/files/ts/consumers/__name@singular__.consumer.ts
+++ src/lib/resource/files/ts/consumers/__name@singular__.consumer.ts
@@ -1,8 +1,5 @@
 import { Processor, Process, OnQueueActive } from '@nestjs/bull';
 import { Job } from 'bull';
-import { <%= singular(classify(name)) %>CreatedEvent } from '../events/<%= singular(name) %>-created.event';
-import { <%= singular(classify(name)) %>UpdatedEvent } from '../events/<%= singular(name) %>-updated.event';
-import { <%= singular(classify(name)) %>RemovedEvent } from '../events/<%= singular(name) %>-removed.event';
 import { EventBus } from '@nestjs/cqrs';
 import { <%= classify(name) %>Service } from '../<%= name %>.service';
 
@@ -14,35 +11,20 @@ export class <%= classify(name) %>Consumer {
   ) {}
 
   @Process('create-<%= singular(name) %>')
-  async create(job: Job) {
+  create(job: Job) {
     const { dto } = job.data;
-    const created = await this.<%= singular(name) %>Service.create(dto);
-    if (created) {
-      job.update({ id: created.id });
-      await job.progress(100);
-      this.eventBus.publish(new <%= singular(classify(name)) %>CreatedEvent(created.id));
-    }
+    this.<%= singular(name) %>Service.create(dto, job);
   }
 
   @Process('update-<%= singular(name) %>')
-  async update(job: Job) {
+  update(job: Job) {
     const { dto, id } = job.data;
-    const updated = await this.<%= singular(name) %>Service.update(id, dto);
-    if (updated) {
-      job.update({ id: updated.id });
-      await job.progress(100);
-      this.eventBus.publish(new <%= singular(classify(name)) %>UpdatedEvent(updated.id));
-    }
+    this.<%= singular(name) %>Service.update(id, dto, job);
   }
 
   @Process('remove-<%= singular(name) %>')
-  async remove(job: Job) {
-    const removed = await this.<%= singular(name) %>Service.remove(job.data.id);
-    if (removed.affected === 1) {
-      await job.progress(100);
-      this.eventBus.publish(new <%= singular(classify(name)) %>RemovedEvent(job.data.id));
-    }
-    await job.discard();
+  remove(job: Job) {
+    this.<%= singular(name) %>Service.remove(job.data.id, job);
   }
 
   @OnQueueActive()
