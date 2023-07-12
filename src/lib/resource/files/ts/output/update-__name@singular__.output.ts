3655a6c3556c3e41626adb9a2b2a58bcb4c5086f
4itech-paul <paul@4idps.com>
2023-07-12 08:57:52 +0800

    fix(res): fix update output type error

--- src/lib/resource/files/ts/output/update-__name@singular__.output.ts
+++ src/lib/resource/files/ts/output/update-__name@singular__.output.ts
@@ -1,8 +1,8 @@
 <% if (type === 'graphql-code-first') { %>import { Field, ObjectType } from '@nestjs/graphql';
-import { NonPositiveIntResolver } from 'graphql-scalars';
+import { NonNegativeIntResolver } from 'graphql-scalars';
 
 @ObjectType()
 export class Update<%= singular(classify(name)) %>Output {
-  @Field(() => NonPositiveIntResolver, { nullable: true })
+  @Field(() => NonNegativeIntResolver, { nullable: true })
   affectedCount?: number;
 }<% } else { %>export class <%= singular(classify(name)) %>Output {}<% } %>
