b32f530566a279118e29e7f41f71ad7d241ccc04
4itech-micky <micky@4idps.com>
2023-07-31 14:49:31 +0800

    fix(remove-input): remove partial type in remove input

--- src/lib/resource/files/ts/input/remove-__name@singular__.input.ts
+++ src/lib/resource/files/ts/input/remove-__name@singular__.input.ts
@@ -1,16 +1,12 @@
-<% if (type === 'graphql-code-first') { %>import { Field, ID, InputType, PartialType } from '@nestjs/graphql';
+<% if (type === 'graphql-code-first') { %>import { Field, ID, InputType } from '@nestjs/graphql';
 
-import { Create<%= singular(classify(name)) %>Input } from './create-<%= singular(name) %>.input';
 
 @InputType()
-export class Remove<%= singular(classify(name)) %>Input extends PartialType(
-  Create<%= singular(classify(name)) %>Input,
-) {
+export class Remove<%= singular(classify(name)) %>Input {
   @Field(() => ID)
   id!: string;
-}<% } else { %>import { Create<%= singular(classify(name)) %>Input } from './create-<%= singular(name) %>.input';
-import { PartialType } from '@nestjs/mapped-types';
+}<% } else { %>
 
-export class Remove<%= singular(classify(name)) %>Input extends PartialType(Create<%= singular(classify(name)) %>Input) {
+export class Remove<%= singular(classify(name)) %>Input {
   id: number;
 }<% } %>
