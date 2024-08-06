86558b85a4fb04b35cd1da8eee6809040fbc01fb
Vahid Najafi <vahid.vdn@gmail.com>
2024-08-06 13:48:06 +0200

    test: update controller.spec to mock service instead of real dependency

--- src/lib/resource/files/ts/__name__.controller.__specFileSuffix__.ts
+++ src/lib/resource/files/ts/__name__.controller.__specFileSuffix__.ts
@@ -4,11 +4,17 @@ import { <%= classify(name) %>Service } from './<%= name %>.service';
 
 describe('<%= classify(name) %>Controller', () => {
   let controller: <%= classify(name) %>Controller;
+  const mockService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [<%= classify(name) %>Controller],
-      providers: [<%= classify(name) %>Service],
+      providers: [
+        {
+          provide: <%= classify(name) %>Service,
+          useValue: mockService,
+        }
+      ],
     }).compile();
 
     controller = module.get<<%= classify(name) %>Controller>(<%= classify(name) %>Controller);
