a369c4f057b09f6ec5dc192991e132b85a23eca9
Vahid Najafi <vahid.vdn@gmail.com>
2024-08-06 19:09:10 +0200

    test: fix unit tests

--- src/lib/resource/resource.factory.test.ts
+++ src/lib/resource/resource.factory.test.ts
@@ -207,11 +207,17 @@ import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  const mockService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockService,
+        }
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -494,11 +500,17 @@ import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  const mockService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockService,
+        }
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
