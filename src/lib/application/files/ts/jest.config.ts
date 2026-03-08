8d85062557a3ec03b7de0653675dc1c00a563231
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2026-03-08 20:06:40 +0100

    chore: move jest configuration out of app package.json

---
+++ src/lib/application/files/ts/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
