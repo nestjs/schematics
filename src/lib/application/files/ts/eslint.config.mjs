76e1b92c79c8b5d083df36e1b35f05054da7bcf8
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-03 19:55:55 +0300

    chore(eslint): refactor ESLint configuration to use defineConfig

--- src/lib/application/files/ts/eslint.config.mjs
+++ src/lib/application/files/ts/eslint.config.mjs
@@ -2,9 +2,10 @@
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -29,7 +30,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
